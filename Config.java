853c78ffc40445ea8518c57ff2fab01c5b53bd73
xor <xor@freenetproject.org>
2008-09-17 13:46:29 +0000

    Comments.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@22667 67a373e5-eb02-0410-a15c-ee090a768436

--- Config.java
+++ Config.java
@@ -10,6 +10,9 @@ import java.util.HashMap;
 import com.db4o.ObjectContainer;
 
 /**
+ * Contains a HashMap<String, String> which maps configuration variable names
+ * to their values and stores them in the database.
+ * 
  * @author Julien Cornuwel (batosai@freenetproject.org)
  */
 public class Config {
@@ -17,6 +20,10 @@ public class Config {
 	private ObjectContainer db;
 	private HashMap<String, String> params = null;
 	
+	/**
+	 * 
+	 * @param db The database where the configuration is stored.
+	 */
 	public Config(ObjectContainer db) {
 		this.db = db;
 		if(params == null) {
@@ -34,6 +41,10 @@ public class Config {
 		return params.get(key);
 	}
 	
+	/**
+	 * Add the default configuration values to the database.
+	 * @param overwrite If true, overwrite already set values with the default value.
+	 */
 	public void initDefault(boolean overwrite) {
 		
 		if (!params.containsKey("delayBetweenInserts") || overwrite) set("delayBetweenInserts", "30");
