bdb8354386a24c7ef4033549be6d8ea0821d3078
Julien Cornuwel <batosai@batosai.net>
2008-10-02 19:02:46 +0000

    Added OwnIdentitiesPage class.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@22914 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ ui/web/OwnIdentitiesPage.java
@@ -0,0 +1,105 @@
+/**
+ * This code is part of WoT, a plugin for Freenet. It is distributed 
+ * under the GNU General Public License, version 2 (or at your option
+ * any later version). See http://www.gnu.org/ for details of the GPL.
+ */
+package plugins.WoT.ui.web;
+
+import java.util.Date;
+
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
+import plugins.WoT.OwnIdentity;
+import plugins.WoT.WoT;
+import freenet.pluginmanager.PluginRespirator;
+import freenet.support.HTMLNode;
+import freenet.support.api.HTTPRequest;
+
+/**
+ * @author Julien Cornuwel (batosai@freenetproject.org)
+ */
+public class OwnIdentitiesPage extends WebPageImpl {
+
+	public OwnIdentitiesPage(WoT wot, HTTPRequest request) {
+		super(wot, request);
+	}
+	
+	public void make() {
+		ObjectContainer db = wot.getDB();
+		PluginRespirator pr = wot.getPR();
+		makeOwnIdentitiesList(db, pr);
+		makeRestoreOwnIdentityForm(pr);
+	}
+	
+	private void makeOwnIdentitiesList(ObjectContainer db, PluginRespirator pr) {
+
+		HTMLNode boxContent = getContentBox("Summary");
+		
+		ObjectSet<OwnIdentity> ownIdentities = OwnIdentity.getAllOwnIdentities(db);
+		if(ownIdentities.size() == 0) {
+			boxContent.addChild("p", "You have no own identity yet, you should create one...");
+		}
+		else {
+			
+			HTMLNode identitiesTable = boxContent.addChild("table", "border", "0");
+			HTMLNode row=identitiesTable.addChild("tr");
+			row.addChild("th", "Name");
+			row.addChild("th", "Last change");
+			row.addChild("th", "Last insert");
+			row.addChild("th", "Publish TrustList ?");
+			row.addChild("th", "Manage");
+			
+			while(ownIdentities.hasNext()) {
+				OwnIdentity id = ownIdentities.next();
+				row=identitiesTable.addChild("tr");
+				row.addChild("td", new String[] {"title", "style"}, new String[] {id.getRequestURI().toString(), "cursor: help;"}, id.getNickName());
+				row.addChild("td",id.getLastChange().toString());
+				HTMLNode cell = row.addChild("td");
+				if(id.getLastInsert() == null) {
+					cell.addChild("p", "Insert in progress...");
+				}
+				else if(id.getLastInsert().equals(new Date(0))) {
+					cell.addChild("p", "Never");
+				}
+				else {
+					cell.addChild(new HTMLNode("a", "href", "/"+id.getRequestURI().toString(), id.getLastInsert().toString()));
+				}
+				row.addChild("td", id.doesPublishTrustList() ? "Yes" : "No");
+				
+				HTMLNode manageCell = row.addChild("td");
+				
+				HTMLNode editForm = pr.addFormChild(manageCell, SELF_URI, "editIdentity");
+				editForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "page", "editIdentity" });
+				editForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "id", id.getRequestURI().toString() });
+				editForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "submit", "edit", "Details" });
+								
+				HTMLNode deleteForm = pr.addFormChild(manageCell, SELF_URI, "deleteIdentity");
+				deleteForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "page", "deleteIdentity" });
+				deleteForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "id", id.getId() });
+				deleteForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "submit", "delete", "Delete" });
+			}
+		}
+	
+		HTMLNode createForm = pr.addFormChild(boxContent, SELF_URI, "createIdentity");
+		createForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "page", "createIdentity" });
+		createForm.addChild("span", new String[] {"title", "style"}, new String[] { "No spaces or special characters.", "border-bottom: 1px dotted; cursor: help;"} , "NickName : ");
+		createForm.addChild("input", new String[] {"type", "name", "size"}, new String[] {"text", "nickName", "30"});
+		createForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "submit", "create", "Create a new identity !" });
+	}
+
+	private void makeRestoreOwnIdentityForm(PluginRespirator pr) {
+		HTMLNode restoreBoxContent = getContentBox("Restore an identity from Freenet");
+		restoreBoxContent.addChild("p", "Use this if you lost your database for some reason (crash, reinstall...) but still have your identity's keys :");
+		
+		HTMLNode restoreForm = pr.addFormChild(restoreBoxContent, SELF_URI, "restoreIdentity");
+		restoreForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "page", "restoreIdentity" });
+		restoreForm.addChild("input", new String[] { "type", "name", "size", "value" }, new String[] { "text", "requestURI", "70", "Request URI" });
+		restoreForm.addChild("br");
+		restoreForm.addChild("input", new String[] { "type", "name", "size", "value" }, new String[] { "text", "insertURI", "70", "InsertURI" });
+		restoreForm.addChild("br");
+		restoreForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "submit", "restore", "Restore this identity !" });
+	
+		restoreBoxContent.addChild("p", "Please don't use that identity (set trust, edit parameters...) until it has been restored from Freenet, or you might loose all its content.");
+	}
+}
