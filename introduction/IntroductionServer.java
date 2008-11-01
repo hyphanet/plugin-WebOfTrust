967f3efacb61d1fa05147bdb08dc1cca02e141e8
xor <xor@freenetproject.org>
2008-11-01 11:46:03 +0000

    Design classes for downloading of puzzles and uploading of solutions. If find a better name for those classes please suggest it.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@23271 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ introduction/IntroductionServer.java
@@ -0,0 +1,17 @@
+/*
+ * This code is part of WoT, a plugin for Freenet. It is distributed 
+ * under the GNU General Public License, version 2 (or at your option
+ * any later version). See http://www.gnu.org/ for details of the GPL.
+ */
+package plugins.WoT.introduction;
+
+/**
+ * This class provides identity announcement for new identities;
+ * It uploads puzzles in certain time intervals and checks whether they were solved.
+ * 
+ * @author xor
+ *
+ */
+public class IntroductionServer {
+
+}
