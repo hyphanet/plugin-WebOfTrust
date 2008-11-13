87095e45c3a3c97780f7b88c9e346ae204794347
xor <xor@freenetproject.org>
2008-11-13 23:52:20 +0000

    Add the "kaptcha" captcha library from http://code.google.com/p/kaptcha/
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@23545 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ introduction/captcha/kaptcha/text/WordRenderer.java
@@ -0,0 +1,11 @@
+package plugins.WoT.introduction.captcha.kaptcha.text;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * {@link WordRenderer} is responsible for rendering words.
+ */
+public interface WordRenderer
+{
+	public BufferedImage renderWord(String word, int width, int height);
+}
