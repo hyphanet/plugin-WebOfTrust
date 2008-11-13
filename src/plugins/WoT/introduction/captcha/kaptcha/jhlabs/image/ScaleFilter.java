64bed479eb0bb650b68ad5f652e28ab09e87bc11
xor <xor@freenetproject.org>
2008-11-13 23:50:46 +0000

    Add image filters from http://www.jhlabs.com/ip/filters/index.html They are needed for our first captcha library.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@23544 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/plugins/WoT/introduction/captcha/kaptcha/jhlabs/image/ScaleFilter.java
@@ -0,0 +1,68 @@
+/*
+Copyright 2006 Jerry Huxtable
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plugins.WoT.introduction.captcha.kaptcha.jhlabs.image;
+
+import java.awt.*;
+import java.awt.image.*;
+
+/**
+ * Scales an image using the area-averaging algorithm, which can't be done with AffineTransformOp.
+ */
+public class ScaleFilter extends AbstractBufferedImageOp {
+
+	private int width;
+	private int height;
+
+    /**
+     * Construct a ScaleFilter.
+     */
+	public ScaleFilter() {
+		this(32, 32);
+	}
+
+    /**
+     * Construct a ScaleFilter.
+     * @param width the width to scale to
+     * @param height the height to scale to
+     */
+	public ScaleFilter( int width, int height ) {
+		this.width = width;
+		this.height = height;
+	}
+
+    public BufferedImage filter( BufferedImage src, BufferedImage dst ) {
+        int w = src.getWidth();
+        int h = src.getHeight();
+
+		if ( dst == null ) {
+			ColorModel dstCM = src.getColorModel();
+			dst = new BufferedImage(dstCM, dstCM.createCompatibleWritableRaster(w, h), dstCM.isAlphaPremultiplied(), null);
+		}
+
+		Image scaleImage = src.getScaledInstance( w, h, Image.SCALE_AREA_AVERAGING );
+		Graphics2D g = dst.createGraphics();
+		g.drawImage( src, 0, 0, width, height, null );
+		g.dispose();
+
+        return dst;
+    }
+
+	public String toString() {
+		return "Distort/Scale";
+	}
+
+}
