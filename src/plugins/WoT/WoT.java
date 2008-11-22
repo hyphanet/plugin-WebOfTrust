5c1fb2c4af95017a2dd08f5df94fb2559d288c88
xor <xor@freenetproject.org>
2008-11-22 18:55:54 +0000

    Move FCP code to a separate class called FCPInterface.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@23815 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/WoT/WoT.java
+++ src/plugins/WoT/WoT.java
@@ -27,6 +27,7 @@ import plugins.WoT.exceptions.UnknownIdentityException;
 import plugins.WoT.introduction.IntroductionClient;
 import plugins.WoT.introduction.IntroductionPuzzle;
 import plugins.WoT.introduction.IntroductionServer;
+import plugins.WoT.ui.fcp.FCPInterface;
 import plugins.WoT.ui.web.ConfigurationPage;
 import plugins.WoT.ui.web.CreateIdentityPage;
 import plugins.WoT.ui.web.HomePage;
@@ -96,6 +97,7 @@ public class WoT implements FredPlugin, FredPluginHTTP, FredPluginThreadless, Fr
 	private Identity seed = null;
 	
 	private WebInterface web;
+	private FCPInterface fcp;
 
 	public void runPlugin(PluginRespirator myPR) {
 		Logger.debug(this, "Start");
@@ -126,7 +128,7 @@ public class WoT implements FredPlugin, FredPluginHTTP, FredPluginThreadless, Fr
 		}
 		*/
 		
-		/* FIXME: debug code, remove before release */
+		/* FIXME: this should be done regularly in IntroductionClient: Puzzles can become corrupted due to the Inserter being deleted from the database */
 		ObjectSet<IntroductionPuzzle> puzzles = db.queryByExample(IntroductionPuzzle.class);
 		for(IntroductionPuzzle p : puzzles) {
 			db.activate(p, 5); /* FIXME: check what happens if we comment this out. or to say it in another way: does querying activate the objects? */
@@ -174,6 +176,7 @@ public class WoT implements FredPlugin, FredPluginHTTP, FredPluginThreadless, Fr
 		}
 		
 		web = new WebInterface(this, SELF_URI);
+		fcp = new FCPInterface(this);
 	}
 	
 	public void terminate() {
@@ -200,6 +203,11 @@ public class WoT implements FredPlugin, FredPluginHTTP, FredPluginThreadless, Fr
 	public String handleHTTPPut(HTTPRequest request) throws PluginHTTPException {
 		return web.handleHTTPPut(request);
 	}
+
+	public void handle(PluginReplySender replysender, SimpleFieldSet params, Bucket data, int accesstype) {
+		fcp.handle(replysender, params, data, accesstype);
+	}
+	
 	
 	public void deleteIdentity(String id) throws DuplicateIdentityException, UnknownIdentityException, DuplicateScoreException, DuplicateTrustException {
 		Identity identity = Identity.getById(db, id);
@@ -391,140 +399,6 @@ public class WoT implements FredPlugin, FredPluginHTTP, FredPluginThreadless, Fr
 		return "0.3.1 r"+Version.getSvnRevision();
 	}
 
-	public void handle(PluginReplySender replysender, SimpleFieldSet params, Bucket data, int accesstype) {
-		
-		try {
-			if(params.get("Message").equals("CreateIdentity")) {
-				replysender.send(handleCreateIdentity(params), data);
-			}
-			else if(params.get("Message").equals("SetTrust")) {
-				replysender.send(handleSetTrust(params), data);
-			}
-			else if(params.get("Message").equals("AddIdentity")) {
-				replysender.send(handleAddIdentity(params), data);
-			}
-			else if(params.get("Message").equals("GetIdentity")) {
-				replysender.send(handleGetIdentity(params), data);
-			}
-			else if(params.get("Message").equals("GetOwnIdentities")) {
-				replysender.send(handleGetOwnIdentities(params), data);
-			}			
-			else if(params.get("Message").equals("GetIdentitiesByScore")) {
-				replysender.send(handleGetIdentitiesByScore(params), data);
-			}			
-			else if(params.get("Message").equals("GetTrusters")) {
-				replysender.send(handleGetTrusters(params), data);
-			}	
-			else if(params.get("Message").equals("GetTrustees")) {
-				replysender.send(handleGetTrustees(params), data);
-			}
-			else if(params.get("Message").equals("AddContext")) {
-				replysender.send(handleAddContext(params), data);
-			}
-			else if(params.get("Message").equals("RemoveContext")) {
-				replysender.send(handleRemoveContext(params), data);
-			}
-			else if(params.get("Message").equals("SetProperty")) {
-				replysender.send(handleSetProperty(params), data);
-			}
-			else if(params.get("Message").equals("GetProperty")) {
-				replysender.send(handleGetProperty(params), data);
-			}
-			else if(params.get("Message").equals("RemoveProperty")) {
-				replysender.send(handleRemoveProperty(params), data);
-			}
-			else {
-				throw new Exception("Unknown message (" + params.get("Message") + ")");
-			}
-		}
-		catch (Exception e) {
-			Logger.error(this, e.toString());
-			replysender.send(errorMessageFCP(e), data);
-		}
-	}
-
-	private SimpleFieldSet handleCreateIdentity(SimpleFieldSet params) throws TransformerConfigurationException, FileNotFoundException, InvalidParameterException, ParserConfigurationException, TransformerException, IOException, InsertException, Db4oIOException, DatabaseClosedException, DuplicateScoreException, NotTrustedException, DuplicateTrustException  {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-		OwnIdentity identity;
-		
-		if(params.get("NickName")==null || params.get("PublishTrustList")==null || params.get("Context")==null) throw new InvalidParameterException("Missing mandatory parameter");
-		
-		if(params.get("RequestURI")==null || params.get("InsertURI")==null) {
-			identity = createIdentity(params.get("NickName"), params.get("PublishTrustList").equals("true"), params.get("Context"));
-		}
-		else {
-			identity = createIdentity(	params.get("InsertURI"),
-										params.get("RequestURI"),
-										params.get("NickName"), 
-										params.get("PublishTrustList").equals("true"),
-										params.get("Context"));
-		}
-		sfs.putAppend("Message", "IdentityCreated");
-		sfs.putAppend("InsertURI", identity.getInsertURI().toString());
-		sfs.putAppend("RequestURI", identity.getRequestURI().toString());	
-		return sfs;
-	}
-
-	private SimpleFieldSet handleSetTrust(SimpleFieldSet params) throws NumberFormatException, TransformerConfigurationException, FileNotFoundException, InvalidParameterException, ParserConfigurationException, TransformerException, IOException, InsertException, UnknownIdentityException, Db4oIOException, DatabaseClosedException, DuplicateScoreException, DuplicateIdentityException, NotTrustedException, DuplicateTrustException  {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("Truster") == null || params.get("Trustee") == null || params.get("Value") == null || params.get("Comment") == null) throw new InvalidParameterException("Missing mandatory parameter");
-		
-		setTrust(params.get("Truster"), params.get("Trustee"), params.get("Value"), params.get("Comment"));
-		
-		sfs.putAppend("Message", "TrustSet");
-		return sfs;
-	}
-	
-	private SimpleFieldSet handleAddIdentity(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, FetchException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("RequestURI") == null) throw new InvalidParameterException("Missing mandatory parameter");
-		
-		Identity identity = addIdentity(params.get("RequestURI").trim());
-		
-		sfs.putAppend("Message", "IdentityAdded");
-		sfs.putAppend("RequestURI", identity.getRequestURI().toString());
-		return sfs;
-	}
-	
-	private SimpleFieldSet handleGetIdentity(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, FetchException, UnknownIdentityException, DuplicateScoreException, DuplicateIdentityException, DuplicateTrustException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("TreeOwner") == null || params.get("Identity") == null) throw new InvalidParameterException("Missing mandatory parameter");
-		
-		sfs.putAppend("Message", "Identity");
-		
-		OwnIdentity treeOwner = OwnIdentity.getByURI(db, params.get("TreeOwner"));
-		Identity identity = Identity.getByURI(db, params.get("Identity"));
-		
-		try {
-			Trust trust = identity.getReceivedTrust(treeOwner, db);
-			sfs.putAppend("Trust", String.valueOf(trust.getValue()));
-		} catch (NotTrustedException e1) {
-			sfs.putAppend("Trust", "null");
-		}  
-		
-		Score score;
-		try {
-			score = identity.getScore(treeOwner, db);
-			sfs.putAppend("Score", String.valueOf(score.getScore()));
-			sfs.putAppend("Rank", String.valueOf(score.getRank()));
-		} catch (NotInTrustTreeException e) {
-			sfs.putAppend("Score", "null");
-			sfs.putAppend("Rank", "null");
-		}
-		
-		Iterator<String> contexts = identity.getContexts();
-		for(int i = 1 ; contexts.hasNext() ; i++) sfs.putAppend("Context"+i, contexts.next());
-		
-		return sfs;
-	}
-	
 	/* would only be needed if we connect the client plugins directly via object references which will probably not happen
 	public List<Identity> getIdentitiesByScore(OwnIdentity treeOwner, int select, String context) throws InvalidParameterException
 	{
@@ -546,176 +420,6 @@ public class WoT implements FredPlugin, FredPluginHTTP, FredPluginThreadless, Fr
 	}
 	*/
 
-	private SimpleFieldSet handleGetOwnIdentities(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, UnknownIdentityException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		sfs.putAppend("Message", "OwnIdentities");
-		
-		ObjectSet<OwnIdentity> result = OwnIdentity.getAllOwnIdentities(db);
-	
-		for(int idx = 1 ; result.hasNext() ; idx++) {
-			OwnIdentity oid = result.next();
-			/* FIXME: Isn't append slower than replace? Figure this out */
-			sfs.putAppend("Identity"+idx, oid.getId());
-			sfs.putAppend("RequestURI"+idx, oid.getRequestURI().toString());
-			sfs.putAppend("InsertURI"+idx, oid.getInsertURI().toString());
-			sfs.putAppend("Nickname"+idx, oid.getNickName());
-			/* FIXME: Allow the client to select what data he wants */
-		}
-		return sfs;
-	}
-	
-	private SimpleFieldSet handleGetIdentitiesByScore(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, UnknownIdentityException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("Select") == null || params.get("Context") == null) throw new InvalidParameterException("Missing mandatory parameter");
-
-		sfs.putAppend("Message", "Identities");
-		
-		OwnIdentity treeOwner = params.get("TreeOwner")!=null ? OwnIdentity.getByURI(db, params.get("TreeOwner")) : null;
-		
-		String selectString = params.get("Select").trim();
-		int select = 0; // TODO: decide about the default value
-		
-		if(selectString.equals("+")) select = 1;
-		else if(selectString.equals("-")) select = -1;
-		else if(selectString.equals("0")) select = 0;
-		else throw new InvalidParameterException("Unhandled select value ("+select+")");
-		
-		ObjectSet<Score> result = Score.getIdentitiesByScore(db, treeOwner, select);
-		String context = params.get("Context");
-		boolean getAll = context.equals("all");
-
-		for(int idx = 1 ; result.hasNext() ; idx++) {
-			Score score = result.next();
-			// TODO: Maybe there is a way to do this through SODA
-			if(getAll || score.getTarget().hasContext(context)) {
-				Identity id = score.getTarget();
-				/* FIXME: Isn't append slower than replace? Figure this out */
-				sfs.putAppend("Identity"+idx, id.getId());
-				sfs.putAppend("RequestURI"+idx, id.getRequestURI().toString());
-				sfs.putAppend("Nickname"+idx, id.getNickName());
-				
-				/* FIXME: Allow the client to select what data he wants */
-			}
-		}
-		return sfs;
-	}
-	
-	private SimpleFieldSet handleGetTrusters(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, UnknownIdentityException, Db4oIOException, DatabaseClosedException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("Identity") == null || params.get("Context") == null) throw new InvalidParameterException("Missing mandatory parameter");
-
-		sfs.putAppend("Message", "Identities");
-		
-		ObjectSet<Trust> result = Identity.getByURI(db, params.get("Identity")).getReceivedTrusts(db);
-	
-		for(int i = 1 ; result.hasNext() ; i++) {
-			Trust trust = result.next();
-			// Maybe there is a way to do this through SODA
-			if(trust.getTruster().hasContext(params.get("Context")) || params.get("Context").equals("all")) {
-				sfs.putAppend("Identity"+i, trust.getTruster().getRequestURI().toString());
-				sfs.putAppend("Value"+i, String.valueOf(trust.getValue()));
-				sfs.putAppend("Comment"+i, trust.getComment());
-			}
-		}
-		return sfs;
-	}
-	
-	private SimpleFieldSet handleGetTrustees(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, UnknownIdentityException, Db4oIOException, DatabaseClosedException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("Identity") == null || params.get("Context") == null) throw new InvalidParameterException("Missing mandatory parameter");
-
-		sfs.putAppend("Message", "Identities");
-
-		ObjectSet<Trust> result = Identity.getByURI(db, params.get("Identity")).getGivenTrusts(db);
-		
-		for(int i = 1 ; result.hasNext() ; i++) {
-			Trust trust = result.next();
-			// Maybe there is a way to do this through SODA
-			if(trust.getTrustee().hasContext(params.get("Context")) || params.get("Context").equals("all")) {
-				sfs.putAppend("Identity"+i, trust.getTrustee().getRequestURI().toString());
-				sfs.putAppend("Value"+i, String.valueOf(trust.getValue()));
-				sfs.putAppend("Comment"+i, trust.getComment());
-			}
-		}
-		return sfs;
-	}
-	
-	private SimpleFieldSet handleAddContext(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, UnknownIdentityException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("Identity") == null || params.get("Context") == null) throw new InvalidParameterException("Missing mandatory parameter");
-
-		addContext(params.get("Identity"), params.get("Context"));
-		
-		sfs.putAppend("Message", "ContextAdded");
-		return sfs;
-	}
-	
-	private SimpleFieldSet handleRemoveContext(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, UnknownIdentityException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("Identity") == null || params.get("Context") == null) throw new InvalidParameterException("Missing mandatory parameter");
-
-		removeContext(params.get("Identity"), params.get("Context"));
-		
-		sfs.putAppend("Message", "ContextRemoved");
-		return sfs;
-	}
-	
-	private SimpleFieldSet handleSetProperty(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, UnknownIdentityException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("Identity") == null || params.get("Property") == null || params.get("Value") == null) throw new InvalidParameterException("Missing mandatory parameter");
-
-		setProperty(params.get("Identity"), params.get("Property"), params.get("Value"));
-		
-		sfs.putAppend("Message", "PropertyAdded");
-		return sfs;
-	}
-
-	private SimpleFieldSet handleGetProperty(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, UnknownIdentityException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("Identity") == null || params.get("Property") == null) throw new InvalidParameterException("Missing mandatory parameter");
-
-		sfs.putAppend("Message", "PropertyValue");
-		sfs.putAppend("Property", getProperty(params.get("Identity"), params.get("Property")));
-		
-		return sfs;
-	}
-
-	private SimpleFieldSet handleRemoveProperty(SimpleFieldSet params) throws InvalidParameterException, MalformedURLException, UnknownIdentityException, DuplicateIdentityException {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-
-		if(params.get("Identity") == null || params.get("Property") == null) throw new InvalidParameterException("Missing mandatory parameter");
-
-		removeProperty(params.get("Identity"), params.get("Property"));
-		
-		sfs.putAppend("Message", "PropertyRemoved");
-		return sfs;
-	}
-	
-	private SimpleFieldSet errorMessageFCP (Exception e) {
-		
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-		sfs.putAppend("Message", "Error");
-		sfs.putAppend("Description", (e.getLocalizedMessage() == null) ? "null" : e.getLocalizedMessage());
-		e.printStackTrace();
-		return sfs;
-	}
 
 	public String getString(String key) {
 		return key;
