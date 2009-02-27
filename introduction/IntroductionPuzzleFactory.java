b2d9d3ed03c8fe2e085828673e9d3f8fbc301f99
xor <xor@freenetproject.org>
2009-02-27 14:10:00 +0000

    Organize imports.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@25857 67a373e5-eb02-0410-a15c-ee090a768436

--- introduction/IntroductionPuzzleFactory.java
+++ introduction/IntroductionPuzzleFactory.java
@@ -6,13 +6,10 @@
 package plugins.WoT.introduction;
 
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
-
-import com.db4o.ObjectContainer;
 
 import plugins.WoT.OwnIdentity;
+
+import com.db4o.ObjectContainer;
 
 /**
  * @author xor
