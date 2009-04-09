f2d2f4498a4ef3dc628e70daba5ae625ea462d9b
Matthew Toseland <toad@amphibian.dyndns.org>
2009-04-09 22:21:36 +0000

    Version WoT
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@26691 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/WoT/Version.java
+++ src/plugins/WoT/Version.java
@@ -12,7 +12,18 @@ package plugins.WoT;
  *
  */
 public class Version {
+	/** SVN revision number. Only set if the plugin is compiled properly e.g. by emu. */
 	private static final String svnRevision = "@custom@";
+
+	/** Version number of the plugin for getRealVersion(). Increment this on making
+	 * a major change, a significant bugfix etc. These numbers are used in auto-update 
+	 * etc, at a minimum any build inserted into auto-update should have a unique 
+	 * version.
+	 * 
+	 * I have set this to 4000 to allow encoding marketing versions into real versions.
+	 * Long provides plenty of leeway! Toad. */
+	public static long version = 4000;
+
 	
 	static String getSvnRevision() {
 		return svnRevision;
@@ -22,6 +33,14 @@ public class Version {
 		System.out.println("=====");
 		System.out.println(svnRevision);
 		System.out.println("=====");		
+	}
+
+	public static String getMarketingVersion() {
+		return "0.4.0 r"+getSvnRevision();
+	}
+
+	public static long getRealVersion() {
+		return version;
 	}
 }
 
