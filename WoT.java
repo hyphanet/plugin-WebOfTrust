06cab7310fd310b2683fffa9266a3077b16ce664
xor <xor@freenetproject.org>
2009-04-09 19:39:29 +0000

    Organize imports.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@26682 67a373e5-eb02-0410-a15c-ee090a768436

--- WoT.java
+++ WoT.java
@@ -10,7 +10,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Random;
 import java.util.Map.Entry;
 
