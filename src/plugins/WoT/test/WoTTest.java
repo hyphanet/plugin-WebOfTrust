8bbb937a2c535e555ada2136339535dc2538dbb5
xor <xor@freenetproject.org>
2009-04-18 16:53:56 +0000

    Compile-fix. All unit tests should succeed now.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@26996 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/WoT/test/WoTTest.java
+++ src/plugins/WoT/test/WoTTest.java
@@ -1,8 +1,6 @@
-/**
- * This code is part of WoT, a plugin for Freenet. It is distributed 
+/* This code is part of WoT, a plugin for Freenet. It is distributed 
  * under the GNU General Public License, version 2 (or at your option
- * any later version). See http://www.gnu.org/ for details of the GPL.
- */
+ * any later version). See http://www.gnu.org/ for details of the GPL. */
 package plugins.WoT.test;
 
 import java.net.MalformedURLException;
@@ -16,11 +14,12 @@ import plugins.WoT.exceptions.DuplicateTrustException;
 import plugins.WoT.exceptions.InvalidParameterException;
 import plugins.WoT.exceptions.NotInTrustTreeException;
 import plugins.WoT.exceptions.NotTrustedException;
+import plugins.WoT.exceptions.UnknownIdentityException;
 
-import com.db4o.ObjectSet;
+import com.db4o.ext.ExtObjectContainer;
 
 /**
- * @author Julien Cornuwel (batosai@freenetproject.org)
+ * @author xor (xor@freenetproject.org), Julien Cornuwel (batosai@freenetproject.org)
  */
 public class WoTTest extends DatabaseBasedTest {
 
@@ -32,230 +31,262 @@ public class WoTTest extends DatabaseBasedTest {
 	 * Otherwise these tests might not be sufficient. 
 	 * Put this logic into the DatabaseBasedTest base class. */
 	
-	public void testInitTrustTree() throws DuplicateScoreException, NotInTrustTreeException, MalformedURLException, InvalidParameterException {
+	public void testInitTrustTree() throws MalformedURLException, InvalidParameterException, UnknownIdentityException, NotInTrustTreeException {
+		mWoT.createOwnIdentity(uriA, uriA, "A", true, "Test"); /* This also initializes the trust tree */
 		
-		OwnIdentity a = new OwnIdentity(uriA, uriA, "A", true);
-		db.store(a);
-		a.initTrustTree(db);
+		assertTrue(mWoT.getAllNonOwnIdentities().size() == 0);
+		assertTrue(mWoT.getAllOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllTrusts().size() == 0);
+		assertTrue(mWoT.getAllScores().size() == 1);
 		
-		assertTrue(Identity.getNbIdentities(db) == 0);
-		assertTrue(OwnIdentity.getNbOwnIdentities(db) == 1);
-		assertTrue(Trust.getNb(db) == 0);
-		assertTrue(Score.getNb(db) == 1);
+		OwnIdentity a = mWoT.getOwnIdentityByURI(uriA);
 
-		assertTrue(a.getScore(a, db).getScore() == 100);
-		assertTrue(a.getScore(a, db).getRank() == 0);
-		assertTrue(a.getScore(a, db).getCapacity() == 100);
-		assertTrue(a.getScore(a, db).getTreeOwner() == a);
-		assertTrue(a.getScore(a, db).getTarget() == a);
+		Score score = mWoT.getScore(a,a);
+		assertTrue(score.getScore() == 100);
+		assertTrue(score.getRank() == 0);
+		assertTrue(score.getCapacity() == 100);
+		assertTrue(score.getTreeOwner() == a);
+		assertTrue(score.getTarget() == a);
 		
 		// Empty the database
+		/* FIXME: This is not needed because DatabaseBasedTest does that in setUp(), right? 
 		ObjectSet<Object> all = db.queryByExample(new Object());
 		while(all.hasNext()) db.delete(all.next());
+		*/
 	}
 	
-	public void testSetTrust() throws DuplicateTrustException, InvalidParameterException, DuplicateScoreException, NotTrustedException, NotInTrustTreeException, MalformedURLException {
-
+	public void testSetTrust1() throws InvalidParameterException, MalformedURLException {
 		OwnIdentity a = new OwnIdentity(uriA, uriA, "A", true);
 		Identity b = new Identity(uriB, "B", true);
-		db.store(a);
-		db.store(b);
+		/* We store them manually so that the WoT does not initialize the trust tree */
+		mWoT.getDB().store(a, 5);
+		mWoT.getDB().store(b, 5);
 		
 		// With A's trust tree not initialized, B shouldn't get a Score.
-		a.setTrust(db, b, (byte)10, "Foo");
+		mWoT.setTrust(a, b, (byte)10, "Foo");
 
-		assertTrue(Identity.getNbIdentities(db) == 1);
-		assertTrue(OwnIdentity.getNbOwnIdentities(db) == 1);
-		assertTrue(Trust.getNb(db) == 1);
-		assertTrue(Score.getNb(db) == 0);
-		
-		// Initialize A's trust tree and set the trust relationship
-		a.initTrustTree(db);
-		a.setTrust(db, b, (byte)100, "Foo");
-		
-		// Check we have the correct number of objects
-		assertTrue(Identity.getNbIdentities(db) == 1);
-		assertTrue(OwnIdentity.getNbOwnIdentities(db) == 1);
-		assertTrue(Trust.getNb(db) == 1);
-		assertTrue(Score.getNb(db) == 2);
-		
-		// Check the Trust object
-		assertTrue(b.getReceivedTrust(a, db).getTruster() == a);
-		assertTrue(b.getReceivedTrust(a, db).getTrustee() == b);
-		assertTrue(b.getReceivedTrust(a, db).getValue() == 100);
-		assertTrue(b.getReceivedTrust(a, db).getComment().equals("Foo"));
-
-		// Check a's Score object
-		assertTrue(a.getScore(a, db).getScore() == 100);
-		assertTrue(a.getScore(a, db).getRank() == 0);
-		assertTrue(a.getScore(a, db).getCapacity() == 100);
-		
-		// Check B's Score object
-		assertTrue(b.getScore(a, db).getScore() == 100);
-		assertTrue(b.getScore(a, db).getRank() == 1);
-		assertTrue(b.getScore(a, db).getCapacity() == 40);
-		
-		// Change the trust value and comment
-		a.setTrust(db, b, (byte)50, "Bar");
-		
-		// Check we have the correct number of objects
-		assertTrue(Identity.getNbIdentities(db) == 1);
-		assertTrue(OwnIdentity.getNbOwnIdentities(db) == 1);
-		assertTrue(Trust.getNb(db) == 1);
-		assertTrue(Score.getNb(db) == 2);
-		
-		// Check the Trust object
-		assertTrue(b.getReceivedTrust(a, db).getTruster() == a);
-		assertTrue(b.getReceivedTrust(a, db).getTrustee() == b);
-		assertTrue(b.getReceivedTrust(a, db).getValue() == 50);
-		assertTrue(b.getReceivedTrust(a, db).getComment().equals("Bar"));
-
-		// Check a's Score object
-		assertTrue(a.getScore(a, db).getScore() == 100);
-		assertTrue(a.getScore(a, db).getRank() == 0);
-		assertTrue(a.getScore(a, db).getCapacity() == 100);
-		
-		// Check B's Score object
-		assertTrue(b.getScore(a, db).getScore() == 50);
-		assertTrue(b.getScore(a, db).getRank() == 1);
-		assertTrue(b.getScore(a, db).getCapacity() == 40);
-		
-		// Empty the database
-		ObjectSet<Object> all = db.queryByExample(new Object());
-		while(all.hasNext()) db.delete(all.next());
+		assertTrue(mWoT.getAllNonOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllTrusts().size() == 1);
+		assertTrue(mWoT.getAllScores().size() == 0);
 	}
 	
-	public void testRemoveTrust() throws MalformedURLException, InvalidParameterException, DuplicateScoreException, DuplicateTrustException, NotTrustedException, NotInTrustTreeException {
-		OwnIdentity a = new OwnIdentity(uriA, uriA, "A", true);
+	public void testSetTrust2() throws MalformedURLException, InvalidParameterException, DuplicateTrustException, NotTrustedException, NotInTrustTreeException {
+
+		OwnIdentity a = mWoT.createOwnIdentity(uriA, uriA, "A", true, "Test"); /* Initializes it's trust tree */
 		Identity b = new Identity(uriB, "B", true);
-		Identity c = new Identity(uriC, "C", true);
-		db.store(a);
+		mWoT.getDB().store(b, 5);
+		
+
+		mWoT.setTrust(a, b, (byte)100, "Foo");
+		
+		// Check we have the correct number of objects
+		assertTrue(mWoT.getAllNonOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllTrusts().size() == 1);
+		assertTrue(mWoT.getAllScores().size() == 2);
+		
+		// Check the Trust object
+		Trust t = mWoT.getTrust(a, b);
+		assertTrue(t.getTruster() == a);
+		assertTrue(t.getTrustee() == b);
+		assertTrue(t.getValue() == 100);
+		assertTrue(t.getComment().equals("Foo"));
+
+		// Check a's Score object
+		Score scoreA = mWoT.getScore(a, a);
+		assertTrue(scoreA.getScore() == 100);
+		assertTrue(scoreA.getRank() == 0);
+		assertTrue(scoreA.getCapacity() == 100);
+		
+		// Check B's Score object
+		Score scoreB = mWoT.getScore(a, b);
+		assertTrue(scoreB.getScore() == 100);
+		assertTrue(scoreB.getRank() == 1);
+		assertTrue(scoreB.getCapacity() == 40);
+		
+		// Change the trust value and comment
+		mWoT.setTrust(a, b, (byte)50, "Bar");
+		
+		// Check we have the correct number of objects
+		assertTrue(mWoT.getAllNonOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllTrusts().size() == 1);
+		assertTrue(mWoT.getAllScores().size() == 2);
+		
+		// Check the Trust object
+		
+		t = mWoT.getTrust(a, b);
+		assertTrue(t.getTruster() == a);
+		assertTrue(t.getTrustee() == b);
+		assertTrue(t.getValue() == 50);
+		assertTrue(t.getComment().equals("Bar"));
+
+		// Check a's Score object
+		scoreA = mWoT.getScore(a, a);
+		assertTrue(scoreA.getScore() == 100);
+		assertTrue(scoreA.getRank() == 0);
+		assertTrue(scoreA.getCapacity() == 100);
+
+		// Check B's Score object
+		scoreB = mWoT.getScore(a, b);
+		assertTrue(scoreB.getScore() == 50);
+		assertTrue(scoreB.getRank() == 1);
+		assertTrue(scoreB.getCapacity() == 40);
+		
+		// Empty the database
+		/* FIXME: This is not needed because DatabaseBasedTest does that in setUp(), right? 
+		ObjectSet<Object> all = db.queryByExample(new Object());
+		while(all.hasNext()) db.delete(all.next());
+		*/
+	}
+	
+	public void testRemoveTrust() throws MalformedURLException, InvalidParameterException, UnknownIdentityException,
+		NotInTrustTreeException {
+		
+		ExtObjectContainer db = mWoT.getDB();
+		
+		OwnIdentity a = mWoT.createOwnIdentity(uriA, uriA, "A", true, "Test");
+		Identity b = new Identity(uriB, "B", true); /* Do not init the trust tree */
+		Identity c = new Identity(uriC, "C", true); /* Do not init the trust tree */
 		db.store(b);
 		db.store(c);
-		a.initTrustTree(db);
-		a.setTrust(db, b, (byte)100, "Foo");
-		b.setTrust(db, c, (byte)50, "Bar");
+		
+		mWoT.setTrust(a, b, (byte)100, "Foo");
+		mWoT.setTrust(b, c, (byte)50, "Bar");
 		
 		// Check we have the correct number of objects
-		assertTrue(OwnIdentity.getNbOwnIdentities(db) == 1);
-		assertTrue(Identity.getNbIdentities(db) == 2);
-		assertTrue(Trust.getNb(db) == 2);
-		assertTrue(Score.getNb(db) == 3);
+		assertTrue(mWoT.getAllOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllNonOwnIdentities().size() == 2);
+		assertTrue(mWoT.getAllTrusts().size() == 2);
+		assertTrue(mWoT.getAllScores().size() == 3);
 		
 		// Check a's Score object
-		assertTrue(a.getScore(a, db).getScore() == 100);
-		assertTrue(a.getScore(a, db).getRank() == 0);
-		assertTrue(a.getScore(a, db).getCapacity() == 100);
+		Score scoreA = mWoT.getScore(a, a);
+		assertTrue(scoreA.getScore() == 100);
+		assertTrue(scoreA.getRank() == 0);
+		assertTrue(scoreA.getCapacity() == 100);
 		
 		// Check B's Score object
-		assertTrue(b.getScore(a, db).getScore() == 100);
-		assertTrue(b.getScore(a, db).getRank() == 1);
-		assertTrue(b.getScore(a, db).getCapacity() == 40);
+		Score scoreB = mWoT.getScore(a, b);
+		assertTrue(scoreB.getScore() == 100);
+		assertTrue(scoreB.getRank() == 1);
+		assertTrue(scoreB.getCapacity() == 40);
 		
 		// Check C's Score object
-		assertTrue(c.getScore(a, db).getScore() == 20);
-		assertTrue(c.getScore(a, db).getRank() == 2);
-		assertTrue(c.getScore(a, db).getCapacity() == 16);
+		Score scoreC = mWoT.getScore(a, c);
+		assertTrue(scoreC.getScore() == 20);
+		assertTrue(scoreC.getRank() == 2);
+		assertTrue(scoreC.getCapacity() == 16);
 		
-		a.setTrust(db, b, (byte)-1, "Bastard");
+		mWoT.setTrust(a, b, (byte)-1, "Bastard");
 		
 		// Check we have the correct number of objects
-		assertTrue(OwnIdentity.getNbOwnIdentities(db) == 1);
-		assertTrue(Identity.getNbIdentities(db) == 2);
-		assertTrue(Trust.getNb(db) == 2);
-		assertTrue(Score.getNb(db) == 2);
+		assertTrue(mWoT.getAllOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllNonOwnIdentities().size() == 2);
+		assertTrue(mWoT.getAllTrusts().size() == 2);
+		assertTrue(mWoT.getAllScores().size() == 2);
 		
 		// Check a's Score object
-		assertTrue(a.getScore(a, db).getScore() == 100);
-		assertTrue(a.getScore(a, db).getRank() == 0);
-		assertTrue(a.getScore(a, db).getCapacity() == 100);
+		scoreA = mWoT.getScore(a, a);
+		assertTrue(scoreA.getScore() == 100);
+		assertTrue(scoreA.getRank() == 0);
+		assertTrue(scoreA.getCapacity() == 100);
 		
 		// Check B's Score object
-		assertTrue(b.getScore(a, db).getScore() == -1);
-		assertTrue(b.getScore(a, db).getRank() == 1);
-		assertTrue(b.getScore(a, db).getCapacity() == 0);
+		scoreB = mWoT.getScore(a, b);
+		assertTrue(scoreB.getScore() == -1);
+		assertTrue(scoreB.getRank() == 1);
+		assertTrue(scoreB.getCapacity() == 0);
 		
 		// C should not have a score anymore
 		try {
-			c.getScore(a, db);
+			mWoT.getScore(a, c);
 			fail();
 		}
 		catch (NotInTrustTreeException e) {}
 		
 		// Empty the database
+		/* FIXME: This is not needed because DatabaseBasedTest does that in setUp(), right? 
 		ObjectSet<Object> all = db.queryByExample(new Object());
 		while(all.hasNext()) db.delete(all.next());
+		*/
 	}
 	
-	public void testTrustLoop() throws MalformedURLException, InvalidParameterException, DuplicateScoreException, DuplicateTrustException, NotInTrustTreeException {
-		OwnIdentity a = new OwnIdentity(uriA, uriA, "A", true);
-		Identity b = new Identity(uriB, "B", true);
-		Identity c = new Identity(uriC, "C", true);
-		db.store(a);
+	public void testTrustLoop() throws MalformedURLException, InvalidParameterException, NotInTrustTreeException {
+		ExtObjectContainer db = mWoT.getDB();
+		
+		OwnIdentity a = mWoT.createOwnIdentity(uriA, uriA, "A", true, "Test");
+		Identity b = new Identity(uriB, "B", true); /* Do not init the trust tree */
+		Identity c = new Identity(uriC, "C", true); /* Do not init the trust tree */
 		db.store(b);
 		db.store(c);
-		a.initTrustTree(db);
-		a.setTrust(db, b, (byte)100, "Foo");
-		b.setTrust(db, c, (byte)50, "Bar");
-		c.setTrust(db, a, (byte)100, "Bleh");
-		c.setTrust(db, b, (byte)50, "Oops");
+		
+		mWoT.setTrust(a, b, (byte)100, "Foo");
+		mWoT.setTrust(b, c, (byte)50, "Bar");
+		mWoT.setTrust(c, a, (byte)100, "Bleh");
+		mWoT.setTrust(c, b, (byte)50, "Oops");
 		
 		// Check we have the correct number of objects
-		assertTrue(OwnIdentity.getNbOwnIdentities(db) == 1);
-		assertTrue(Identity.getNbIdentities(db) == 2);
-		assertTrue(Trust.getNb(db) == 4);
-		assertTrue(Score.getNb(db) == 3);
-		
+		assertTrue(mWoT.getAllOwnIdentities().size() == 1);
+		assertTrue(mWoT.getAllNonOwnIdentities().size() == 2);
+		assertTrue(mWoT.getAllTrusts().size() == 4);
+		assertTrue(mWoT.getAllScores().size() == 3);
+
 		// Check a's Score object
-		assertTrue(a.getScore(a, db).getScore() == 100);
-		assertTrue(a.getScore(a, db).getRank() == 0);
-		assertTrue(a.getScore(a, db).getCapacity() == 100);
+		Score scoreA = mWoT.getScore(a, a);
+		assertTrue(scoreA.getScore() == 100);
+		assertTrue(scoreA.getRank() == 0);
+		assertTrue(scoreA.getCapacity() == 100);
 		
 		// Check B's Score object
-		assertTrue(b.getScore(a, db).getScore() == 108);
-		assertTrue(b.getScore(a, db).getRank() == 1);
-		assertTrue(b.getScore(a, db).getCapacity() == 40);
+		Score scoreB = mWoT.getScore(a, b);
+		assertTrue(scoreB.getScore() == 108);
+		assertTrue(scoreB.getRank() == 1);
+		assertTrue(scoreB.getCapacity() == 40);
 		
 		// Check C's Score object
-		assertTrue(c.getScore(a, db).getScore() == 20);
-		assertTrue(c.getScore(a, db).getRank() == 2);
-		assertTrue(c.getScore(a, db).getCapacity() == 16);
+		Score scoreC = mWoT.getScore(a, c);
+		assertTrue(scoreC.getScore() == 20);
+		assertTrue(scoreC.getRank() == 2);
+		assertTrue(scoreC.getCapacity() == 16);
 	}
 	
-	public void testOwnIndentitiesTrust() throws MalformedURLException, InvalidParameterException, DuplicateScoreException, DuplicateTrustException, NotTrustedException, NotInTrustTreeException {
-		OwnIdentity a = new OwnIdentity(uriA, uriA, "A", true);
-		OwnIdentity b = new OwnIdentity(uriB, uriB, "B", true);
-		db.store(a);
-		db.store(b);
-		a.initTrustTree(db);
-		b.initTrustTree(db);
-		a.setTrust(db, b, (byte)100, "Foo");
-		b.setTrust(db, a, (byte)100, "Bar");
+	public void testOwnIndentitiesTrust() throws MalformedURLException, InvalidParameterException, NotInTrustTreeException {
+		OwnIdentity a = mWoT.createOwnIdentity(uriA, uriA, "A", true, "Test");
+		OwnIdentity b = mWoT.createOwnIdentity(uriB, uriB, "B", true, "Test");
+
+		mWoT.setTrust(a, b, (byte)100, "Foo");
+		mWoT.setTrust(b, a, (byte)100, "Bar");
 		
 		// Check we have the correct number of objects
-		assertTrue(OwnIdentity.getNbOwnIdentities(db) == 2);
-		assertTrue(Identity.getNbIdentities(db) == 0);
-		assertTrue(Trust.getNb(db) == 2);
-		assertTrue(Score.getNb(db) == 4);
+		assertTrue(mWoT.getAllOwnIdentities().size() == 2);
+		assertTrue(mWoT.getAllNonOwnIdentities().size() == 0);
+		assertTrue(mWoT.getAllTrusts().size() == 2);
+		assertTrue(mWoT.getAllScores().size() == 4);
 		
 		// Check a's own Score object
-		assertTrue(a.getScore(a, db).getScore() == 100);
-		assertTrue(a.getScore(a, db).getRank() == 0);
-		assertTrue(a.getScore(a, db).getCapacity() == 100);
+		Score scoreA = mWoT.getScore(a, a);
+		assertTrue(scoreA.getScore() == 100);
+		assertTrue(scoreA.getRank() == 0);
+		assertTrue(scoreA.getCapacity() == 100);
 		
 		// Check a's Score object
-		assertTrue(a.getScore(b, db).getScore() == 100);
-		assertTrue(a.getScore(b, db).getRank() == 1);
-		assertTrue(a.getScore(b, db).getCapacity() == 40);
+		Score scoreAfromB = mWoT.getScore(b, a);
+		assertTrue(scoreAfromB.getScore() == 100);
+		assertTrue(scoreAfromB.getRank() == 1);
+		assertTrue(scoreAfromB.getCapacity() == 40);
 				
 		// Check B's own Score object
-		assertTrue(b.getScore(b, db).getScore() == 100);
-		assertTrue(b.getScore(b, db).getRank() == 0);
-		assertTrue(b.getScore(b, db).getCapacity() == 100);
+		Score scoreB = mWoT.getScore(a, a);
+		assertTrue(scoreB.getScore() == 100);
+		assertTrue(scoreB.getRank() == 0);
+		assertTrue(scoreB.getCapacity() == 100);
 
 		// Check B's Score object
-		assertTrue(b.getScore(a, db).getScore() == 100);
-		assertTrue(b.getScore(a, db).getRank() == 1);
-		assertTrue(b.getScore(a, db).getCapacity() == 40);
+		Score scoreBfromA = mWoT.getScore(b, a);
+		assertTrue(scoreBfromA.getScore() == 100);
+		assertTrue(scoreBfromA.getRank() == 1);
+		assertTrue(scoreBfromA.getCapacity() == 40);
 	}
 }
