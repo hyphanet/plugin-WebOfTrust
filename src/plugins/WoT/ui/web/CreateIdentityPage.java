34dfcb340ce6a931babfa4065d7fad8a53be50f4
xor <xor@freenetproject.org>
2009-04-19 15:09:11 +0000

    Refactor the webinterface: Move all page-related code to a separate class for each page, synchronize the webinterface, fix some bugs, clean up, etc. Too many changes to list them all, sorry.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/WoT@27049 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/WoT/ui/web/CreateIdentityPage.java
+++ src/plugins/WoT/ui/web/CreateIdentityPage.java
@@ -1,22 +1,18 @@
-/**
- * This code is part of WoT, a plugin for Freenet. It is distributed 
+/* This code is part of WoT, a plugin for Freenet. It is distributed 
  * under the GNU General Public License, version 2 (or at your option
- * any later version). See http://www.gnu.org/ for details of the GPL.
- */
-
+ * any later version). See http://www.gnu.org/ for details of the GPL. */
 package plugins.WoT.ui.web;
 
-import freenet.client.HighLevelSimpleClient;
+import plugins.WoT.OwnIdentity;
 import freenet.keys.FreenetURI;
-import freenet.pluginmanager.PluginRespirator;
 import freenet.support.HTMLNode;
 import freenet.support.api.HTTPRequest;
-import plugins.WoT.WoT;
 
 /**
  * The page the user can create an OwnIdentity.
  * 
- * @author Julien Cornuwel (batosai@freenetproject.org), xor (xor@freenetproject.org)
+ * @author xor (xor@freenetproject.org)
+ * @author Julien Cornuwel (batosai@freenetproject.org)
  */
 public class CreateIdentityPage extends WebPageImpl {
 
@@ -29,15 +25,26 @@ public class CreateIdentityPage extends WebPageImpl {
 	public CreateIdentityPage(WebInterface myWebInterface, HTTPRequest myRequest) {
 		super(myWebInterface, myRequest);
 	}
-	
-	/* (non-Javadoc)
-	 * @see plugins.WoT.ui.web.WebPage#make()
-	 */
+
 	public void make() {
-		PluginRespirator pr = wot.getPluginRespirator();
-		HighLevelSimpleClient client = pr.getHLSimpleClient();
-		
-		makeCreateForm(client, pr, request.getPartAsString("nickName",1024));
+		if(request.isPartSet("CreateIdentity")) {
+			try {
+				OwnIdentity newIdentity =
+					wot.createOwnIdentity(request.getPartAsString("InsertURI",1024), request.getPartAsString("RequestURI",1024),
+										request.getPartAsString("Nickname", 1024), request.getPartAsString("PublishTrustList", 5).equals("true"),
+										"Freetalk"); /* FIXME: Freetalk should do that itself! */
+				
+				/* FIXME: inline the own identities page. first we need to modify our base class to be able to do so, see freetalk */
+				
+				addContentBox("Your identity was created.").addChild("#", "Please go to the own identities page and solve " +
+						"introduction puzzles, otherwise nobody will see the identity!");
+				
+			} catch (Exception e) {
+				addErrorBox("Identity creation failed", e.getMessage());
+			}	
+		}
+		else
+			makeCreateForm(request.getPartAsString("Nickname",1024));
 	}
 	
 	/**
@@ -47,23 +54,23 @@ public class CreateIdentityPage extends WebPageImpl {
 	 * @param pr a reference to the PluginRespirator
 	 * @param nickName the nickName supplied by the user
 	 */
-	private void makeCreateForm(HighLevelSimpleClient client, PluginRespirator pr, String nickName) {
-		HTMLNode boxContent = getContentBox("Identity creation");
-		FreenetURI[] keypair = client.generateKeyPair("WoT");
+	private void makeCreateForm(String nickName) {
+		HTMLNode boxContent = addContentBox("Identity creation");
+		FreenetURI[] keypair = wot.getPluginRespirator().getHLSimpleClient().generateKeyPair("WoT");
 		
-		HTMLNode createForm = pr.addFormChild(boxContent, SELF_URI, "createIdentity2");
-		createForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "page", "createIdentity2" });
+		HTMLNode createForm = pr.addFormChild(boxContent, uri, "CreateIdentity");
+		createForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "page", "CreateIdentity" });
 		createForm.addChild("#", "Request URI : ");
-		createForm.addChild("input", new String[] { "type", "name", "size", "value" }, new String[] { "text", "requestURI", "70", keypair[1].toString() });
+		createForm.addChild("input", new String[] { "type", "name", "size", "value" }, new String[] { "text", "RequestURI", "70", keypair[1].toString() });
 		createForm.addChild("br");
 		createForm.addChild("#", "Insert URI : ");
-		createForm.addChild("input", new String[] { "type", "name", "size", "value" }, new String[] { "text", "insertURI", "70", keypair[0].toString() });
+		createForm.addChild("input", new String[] { "type", "name", "size", "value" }, new String[] { "text", "InsertURI", "70", keypair[0].toString() });
 		createForm.addChild("br");
 		createForm.addChild("#", "Publish trust list ");
-		createForm.addChild("input", new String[] { "type", "name", "value", "checked" }, new String[] { "checkbox", "publishTrustList", "true", "checked"});
+		createForm.addChild("input", new String[] { "type", "name", "value", "checked" }, new String[] { "checkbox", "PublishTrustList", "true", "checked"});
 		createForm.addChild("br");
-		createForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "nickName", nickName });
-		createForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "submit", "create", "Create a new identity !" });
+		createForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "hidden", "Nickname", nickName });
+		createForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "submit", "CreateIdentity", "Create it" });
 
 	}
 
