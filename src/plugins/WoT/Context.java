b9bc0b4b110938ccc4015d851b85eefaac01b31b
xor <xor@freenetproject.org>
2009-05-05 00:10:22 +0200

    - Add some more javadoc to class Context. - Add the TrustList context to the allowed contexts - Limit context name length to 256.

--- src/plugins/WoT/Context.java
+++ src/plugins/WoT/Context.java
@@ -5,7 +5,18 @@ package plugins.WoT;
 
 
 /**
- * A context is a service which an identity can provide, for example a client application such as "Freetalk".
+ * A context is a service which an identity can provide.
+ * 
+ * Contexts are used by client applications to identify what identities are relevant for their use.
+ * 
+ * Each context has its own sub-web of trust:
+ * - If an identity assigns a trust value to another identity, it can specify to which context the trust value belongs.
+ * - The WoT-plugin calculates per-identity {@link Score} values for every context from the given trust values in that context.
+ * 
+ * Currently allowed contexts:
+ * - WoT adds the "TrustList" context if an identity publishes a trust list.
+ * - WoT adds the "Introduction" context if an identity publishes catpchas to allow other to get on it's trust list.
+ * - Freetalk, the messaging system for Freenet, adds the "Freetalk" context to identities which use it.
  * 
  * The database stores exactly 1 object of type Context for every allowed Context.
  * Contexts are linked to identities by storying a {@link ContextOffer} object per "identity X offers context Y"
@@ -18,7 +29,7 @@ public final class Context {
 	/**
 	 * List of context names which WoT will allow, stored in the Config object upon creation of a new database.
 	 */
-	public static final String DEFAULT_ALLOWED_CONTEXT_NAMES = "Introduction, Freetalk";
+	public static final String DEFAULT_ALLOWED_CONTEXT_NAMES = "TrustList,Introduction,Freetalk";
 	
 	private final String mName;
 	
@@ -30,6 +41,9 @@ public final class Context {
 	public Context(String myName) {
 		if(myName.length() == 0)
 			throw new IllegalArgumentException("Empty context names are not permitted.");
+		
+		if(myName.length() > 256)
+			throw new IllegalArgumentException("Context names must be no longer than 256 characters.");
 		
 		if(!myName.matches("[A-Za-z]"))
 			throw new IllegalArgumentException("Context names must only contain the characters A to Z.");
