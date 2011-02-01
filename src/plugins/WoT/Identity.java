42c67457457e4f60e1bb2560349176fce5de5c9d
xor <xor@freenetproject.org>
2011-02-01 18:45:00 +0100

    Fix glitch in property name validation

--- src/plugins/WoT/Identity.java
+++ src/plugins/WoT/Identity.java
@@ -533,7 +533,7 @@ public class Identity implements Cloneable {
 			throw new InvalidParameterException("Property names must not be longer than " + MAX_PROPERTY_NAME_LENGTH + " characters.");
 		}
 		
-		String[] keyTokens = key.split("[.]");
+		String[] keyTokens = key.split("[.]", -1); // The 1-argument-version wont return empty tokens
 		for (String token : keyTokens) {
 			if (token.length() == 0) {
 				throw new InvalidParameterException("Property names which contain periods must have at least one character before and after each period.");
