74453bda76f8fd138612dc0e4583914a0ab1b682
xor <xor@freenetproject.org>
2011-02-26 20:00:18 +0100

    Imports

--- src/plugins/WebOfTrust/introduction/IntroductionPuzzleStore.java
+++ src/plugins/WebOfTrust/introduction/IntroductionPuzzleStore.java
@@ -1,10 +1,7 @@
 package plugins.WebOfTrust.introduction;
 
 import java.text.ParseException;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
 
 import plugins.WebOfTrust.Identity;
 import plugins.WebOfTrust.OwnIdentity;
