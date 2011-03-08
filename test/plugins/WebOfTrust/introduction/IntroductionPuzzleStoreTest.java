3b503705e6116c80389bf0df995fb9e573984279
xor <xor@freenetproject.org>
2011-03-08 18:58:58 +0100

    Work on IntroductionPuzzleStoreTest

--- test/plugins/WebOfTrust/introduction/IntroductionPuzzleStoreTest.java
+++ test/plugins/WebOfTrust/introduction/IntroductionPuzzleStoreTest.java
@@ -1,6 +1,7 @@
 package plugins.WebOfTrust.introduction;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
@@ -34,16 +35,18 @@ public final class IntroductionPuzzleStoreTest extends DatabaseBasedTest {
 		
 		final String uriA = "SSK@MF2Vc6FRgeFMZJ0s2l9hOop87EYWAydUZakJzL0OfV8,fQeN-RMQZsUrDha2LCJWOMFk1-EiXZxfTnBT8NEgY00,AQACAAE/";
 		final String uriB = "SSK@R3Lp2s4jdX-3Q96c0A9530qg7JsvA9vi2K0hwY9wG-4,ipkgYftRpo0StBlYkJUawZhg~SO29NZIINseUtBhEfE,AQACAAE/";
+		final String uriC = "SSK@qd-hk0vHYg7YvK2BQsJMcUD5QSF0tDkgnnF6lnWUH0g,xTFOV9ddCQQk6vQ6G~jfL6IzRUgmfMcZJ6nuySu~NUc,AQACAAE/";
 		
 		mOwnIdentities = new ArrayList<OwnIdentity>();
 		mOwnIdentities.add(mWoT.createOwnIdentity(uriA, uriA, "B", true, "Test"));
 		mOwnIdentities.add(mWoT.createOwnIdentity(uriB, uriB, "B", true, "Test"));
+		mOwnIdentities.add(mWoT.createOwnIdentity(uriC, uriC, "C", true, "Test"));
 		mOwnIdentities = Collections.unmodifiableList(mOwnIdentities);
 		
 		mOwnIdentity = mOwnIdentities.get(0);
 	
 		assertEquals(1, mPuzzleFactories.size());
-		assertEquals(2, mOwnIdentities.size());
+		assertEquals(3, mOwnIdentities.size());
 		assertEquals(0, mPuzzleStore.getOwnCatpchaAmount(false));
 		assertEquals(0, mPuzzleStore.getOwnCatpchaAmount(true));
 	}
@@ -66,6 +69,15 @@ public final class IntroductionPuzzleStoreTest extends DatabaseBasedTest {
 		
 		flushCaches();
 		
+		return Collections.unmodifiableList(result);
+	}
+	
+	/**
+	 * TODO: There seems to be no java API for this. Sucks. Such simple stuff should only be used from libraroes-
+	 */
+	private List<OwnIntroductionPuzzle> deepCopy(final List<OwnIntroductionPuzzle> puzzles) {
+		final ArrayList<OwnIntroductionPuzzle> result = new ArrayList<OwnIntroductionPuzzle>(puzzles.size() + 1);
+		for(OwnIntroductionPuzzle p : puzzles) result.add(p.clone());
 		return Collections.unmodifiableList(result);
 	}
 	
@@ -188,11 +200,17 @@ public final class IntroductionPuzzleStoreTest extends DatabaseBasedTest {
 	}
 
 	public void testGetByID() throws IOException, UnknownPuzzleException {
-		for(OwnIntroductionPuzzle puzzle : generateNewPuzzles(mOwnIdentity)) {
+		final List<OwnIntroductionPuzzle> puzzles = generateNewPuzzles(mOwnIdentity);
+		
+		for(final OwnIntroductionPuzzle puzzle : puzzles) {
 			assertSame(puzzle, mPuzzleStore.getByID(puzzle.getID()));
 		}
 		
-		// FIXME: Also test whether the query works if we clone them - are the returned ones .equals()?
+		for(final OwnIntroductionPuzzle clone : deepCopy(puzzles)) {
+			final IntroductionPuzzle original = mPuzzleStore.getByID(clone.getID());
+			assertNotSame(clone, original);
+			assertEquals(clone, original);
+		}
 		
 		try {
 			mPuzzleStore.getByID(UUID.randomUUID().toString());
@@ -200,20 +218,74 @@ public final class IntroductionPuzzleStoreTest extends DatabaseBasedTest {
 		} catch(UnknownPuzzleException e) {} 
 	}
 
-	public void testGetPuzzleBySolutionURI() {
-		// FIXME: Implement
+	public void testGetPuzzleBySolutionURI() throws ParseException, UnknownIdentityException, UnknownPuzzleException, IOException {
+		final List<OwnIntroductionPuzzle> puzzles = generateNewPuzzles(mOwnIdentity);
+		
+		for(final IntroductionPuzzle puzzle : puzzles) {
+			assertSame(puzzle, mPuzzleStore.getPuzzleBySolutionURI(puzzle.getSolutionURI()));
+		}
+		
+		for(final IntroductionPuzzle clone : deepCopy(puzzles)) {
+			final IntroductionPuzzle original = mPuzzleStore.getPuzzleBySolutionURI(clone.getSolutionURI());
+			assertNotSame(clone, original);
+			assertEquals(clone, original);
+		}
 	}
 
-	public void testGetOwnPuzzleByRequestURI() {
-		// FIXME: Implement
+	public void testGetOwnPuzzleByRequestURI() throws IOException, ParseException, UnknownIdentityException, UnknownPuzzleException {
+		final List<OwnIntroductionPuzzle> puzzles = generateNewPuzzles(mOwnIdentity);
+		
+		for(final OwnIntroductionPuzzle puzzle : puzzles) {
+			assertSame(puzzle, mPuzzleStore.getOwnPuzzleByRequestURI(puzzle.getRequestURI()));
+		}
+		
+		for(final OwnIntroductionPuzzle clone : deepCopy(puzzles)) {
+			final IntroductionPuzzle original = mPuzzleStore.getOwnPuzzleByRequestURI(clone.getRequestURI());
+			assertNotSame(clone, original);
+			assertEquals(clone, original);
+		}
 	}
 
-	public void testGetOwnPuzzleBySolutionURI() {
-		// FIXME: Implement
+	public void testGetOwnPuzzleBySolutionURI() throws ParseException, UnknownPuzzleException, IOException {
+		final List<OwnIntroductionPuzzle> puzzles = generateNewPuzzles(mOwnIdentity);
+		
+		for(final IntroductionPuzzle puzzle : puzzles) {
+			assertSame(puzzle, mPuzzleStore.getOwnPuzzleBySolutionURI(puzzle.getSolutionURI()));
+		}
+		
+		for(final IntroductionPuzzle clone : deepCopy(puzzles)) {
+			final IntroductionPuzzle original = mPuzzleStore.getOwnPuzzleBySolutionURI(clone.getSolutionURI());
+			assertNotSame(clone, original);
+			assertEquals(clone, original);
+		}
 	}
 
-	public void testGetFreeIndex() {
-		// FIXME: Implement
+	public void testGetFreeIndex() throws IOException {
+		int[] freeIndices = new int[mOwnIdentities.size()];
+		
+		for(int i=0; i < mOwnIdentities.size(); ++i) {
+			freeIndices[i] = 0;
+			
+			// 0. identity => 0x generateNewPuzzles
+			// 1. identity => 1x generateNewPuzzles
+			// 2. identity => 2x generateNewPuzzles
+			for(int j=i; j > 0; --j) {
+				for(OwnIntroductionPuzzle p : generateNewPuzzles(mOwnIdentities.get(i))) {
+					freeIndices[i] = Math.max(freeIndices[i], p.getIndex() + 1);
+				}
+			}
+			
+			// To check whether the free index of other days does not make the free index of today go wrong, we store a puzzle for a different day for each identity.
+			mPuzzleStore.storeAndCommit(constructPuzzle(mOwnIdentities.get(i), new Date(CurrentTimeUTC.getInMillis() + 10 * IntroductionServer.PUZZLE_INVALID_AFTER_DAYS * 10 * 24 * 60 * 60 * 1000)));
+		}
+		
+		final Date date = CurrentTimeUTC.get();
+		final Date nullDate = new Date(0);
+	
+		for(int i=0; i < mOwnIdentities.size(); ++i) {
+			assertEquals(freeIndices[i], mPuzzleStore.getFreeIndex(mOwnIdentities.get(i), date));
+			assertEquals(0, mPuzzleStore.getFreeIndex(mOwnIdentities.get(i), nullDate));
+		}
 	}
 
 	public void testGetUninsertedOwnPuzzlesByInserter() {
