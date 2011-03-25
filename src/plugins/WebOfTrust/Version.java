c697101a5898f7b27e8e1954978f3d9140d14709
xor <xor@freenetproject.org>
2011-03-25 21:34:52 +0100

    Update version string to RC4. Do not update anything else by purpose. The long-version will be reset once final is out anyway.

--- src/plugins/WebOfTrust/Version.java
+++ src/plugins/WebOfTrust/Version.java
@@ -31,7 +31,7 @@ public class Version {
 	public static final long latestVersion = version;
 
 	public static String getMarketingVersion() {
-		return "0.4.0 RC3 "+gitRevision;
+		return "0.4.0 RC4 "+gitRevision;
 	}
 
 	public static long getRealVersion() {
