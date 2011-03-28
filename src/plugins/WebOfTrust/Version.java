316ead6e92e397cc766a41109eabdfdf29a0086d
xor <xor@freenetproject.org>
2011-03-28 22:36:41 +0200

    Set version.

--- src/plugins/WebOfTrust/Version.java
+++ src/plugins/WebOfTrust/Version.java
@@ -19,19 +19,17 @@ public class Version {
 	 * a major change, a significant bugfix etc. These numbers are used in auto-update 
 	 * etc, at a minimum any build inserted into auto-update should have a unique 
 	 * version.
-	 * 
-	 * I have set this to 4000 to allow encoding marketing versions into real versions.
-	 * Long provides plenty of leeway! Toad. */
-	public static final long version = 4004; // FIXME: Don't forget to adjust when we release 0.4  final!
+	 */
+	public static final long version = 1;
 	
 	/** Published as an identity property if you own a seed identity. */
-	public static final long mandatoryVersion = 4004; // FIXME: Don't forget to adjust when we release 0.4  final!
+	public static final long mandatoryVersion = 1;
 	
 	/** Published as an identity property if you own a seed identity. */
 	public static final long latestVersion = version;
 
 	public static String getMarketingVersion() {
-		return "0.4.0 RC4 "+gitRevision;
+		return "0.4.0 "+gitRevision;
 	}
 
 	public static long getRealVersion() {
