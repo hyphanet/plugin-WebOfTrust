038457e661abf8693b13577c0acaf8aef2bbbf11
xor <xor@freenetproject.org>
2011-04-03 21:01:56 +0200

    Add TheSeeker as seed identity.

--- src/plugins/WebOfTrust/WebOfTrust.java
+++ src/plugins/WebOfTrust/WebOfTrust.java
@@ -86,6 +86,7 @@ public class WebOfTrust implements FredPlugin, FredPluginThreadless, FredPluginF
 		"USK@QeTBVWTwBldfI-lrF~xf0nqFVDdQoSUghT~PvhyJ1NE,OjEywGD063La2H-IihD7iYtZm3rC0BP6UTvvwyF5Zh4,AQACAAE/WebOfTrust/0", // xor
 		"USK@z9dv7wqsxIBCiFLW7VijMGXD9Gl-EXAqBAwzQ4aq26s,4Uvc~Fjw3i9toGeQuBkDARUV5mF7OTKoAhqOA9LpNdo,AQACAAE/WebOfTrust/0", // Toad
 		"USK@e3myoFyp5avg6WYN16ImHri6J7Nj8980Fm~aQe4EX1U,QvbWT0ImE0TwLODTl7EoJx2NBnwDxTbLTE6zkB-eGPs,AQACAAE/WebOfTrust/0", // Bombe
+		"USK@cI~w2hrvvyUa1E6PhJ9j5cCoG1xmxSooi7Nez4V2Gd4,A3ArC3rrJBHgAJV~LlwY9kgxM8kUR2pVYXbhGFtid78,AQACAAE/WebOfTrust/19", // TheSeeker
 	};
 
 	/* References from the node */
