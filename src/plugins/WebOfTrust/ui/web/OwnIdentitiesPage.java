d35820edc7d38a9fbb3dab5099a45563b1d1fd4e
Arne Babenhauserheide <bab@draketo.de>
2011-10-18 10:05:46 +0200

    use the protected space name in the OwnIdentitiesPage

--- src/plugins/WebOfTrust/ui/web/OwnIdentitiesPage.java
+++ src/plugins/WebOfTrust/ui/web/OwnIdentitiesPage.java
@@ -145,7 +145,7 @@ public class OwnIdentitiesPage extends WebPageImpl {
 		createForm.addChild("span", new String[] { "title", "style" }, 
 				new String[] { l10n().getString("OwnIdentitiesPage.OwnIdentities.Nickname.Tooltip"), "border-bottom: 1px dotted; cursor: help;"}, 
 		        l10n().getString("OwnIdentitiesPage.OwnIdentities.Nickname") + " : ");
-		createForm.addChild("input", new String[] { "type", "name", "size", "value" }, new String[] {"text", "Nickname", "30", RandomName.newNickname()});
+		createForm.addChild("input", new String[] { "type", "name", "size", "value" }, new String[] {"text", "Nickname", "30", RandomName.newName()});
 		createForm.addChild("input", new String[] { "type", "name", "value" }, new String[] { "submit", "create", l10n().getString("OwnIdentitiesPage.OwnIdentities.CreateButton") });
 	}
 
