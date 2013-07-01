hyphanet/plugin-WebOfTrust

commit 5685ed39131c19d697ec3430704c139c67f0a861

Resolve TODO of moving a function to the proper class

src/plugins/WebOfTrust/Identity.java
@@ -146,6 +146,20 @@ public class Identity extends Persistent implements Cloneable {
 			return false;
 		}
 
+		/**
+		 * Generates a unique IDfrom a {@link FreenetURI}, which is the routing key of the author encoded with the Freenet-variant of Base64
+		 * We use this to identify identities and perform requests on the database. 
+		 * 
+		 * TODO: Return an IdentityID, not a String
+		 * 
+		 * @param uri The requestURI of the Identity
+		 * @return A string to uniquely identify the identity.
+		 */
+		public static final String getIDFromURI(FreenetURI uri) {
+			/* WARNING: When changing this, also update Freetalk.WoT.WoTIdentity.getUIDFromURI()! */
+			return Base64.encode(uri.getRoutingKey());
+		}
+
 	}
 	
 	
@@ -170,7 +184,7 @@ public class Identity extends Persistent implements Cloneable {
 		//Check that mRequestURI really is a request URI
 		USK.create(mRequestURI);
 		
-		mID = getIDFromURI(mRequestURI);
+		mID = IdentityID.getIDFromURI(mRequestURI);
 		
 		try {
 			mLatestEditionHint = newRequestURI.getEdition();
@@ -222,20 +236,6 @@ public class Identity extends Persistent implements Cloneable {
 		return mID;
 	}
 
-	/**
-	 * Generates a unique IDfrom a {@link FreenetURI}, which is the routing key of the author encoded with the Freenet-variant of Base64
-	 * We use this to identify identities and perform requests on the database. 
-	 * 
-	 * TODO: Move to class IdentityID.
-	 * 
-	 * @param uri The requestURI of the Identity
-	 * @return A string to uniquely identify the identity.
-	 */
-	public static final String getIDFromURI(FreenetURI uri) {
-		/* WARNING: When changing this, also update Freetalk.WoT.WoTIdentity.getUIDFromURI()! */
-		return Base64.encode(uri.getRoutingKey());
-	}
-	
 	/**
 	 * TODO: Move to class IdentityID.
 	 */
@@ -908,7 +908,7 @@ public class Identity extends Persistent implements Cloneable {
 		if(mRequestURI == null)
 			throw new NullPointerException("mRequestURI==null");
 		
-		if(!mID.equals(getIDFromURI(mRequestURI)))
+		if(!mID.equals(IdentityID.getIDFromURI(mRequestURI)))
 			throw new IllegalStateException("ID does not match request URI!");
 		
 		IdentityID.constructAndValidate(mID); // Throws if invalid

src/plugins/WebOfTrust/WebOfTrust.java
@@ -12,6 +12,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Random;
 
+import plugins.WebOfTrust.Identity.IdentityID;
 import plugins.WebOfTrust.Score.ScoreID;
 import plugins.WebOfTrust.Trust.TrustID;
 import plugins.WebOfTrust.exceptions.DuplicateIdentityException;
@@ -1451,7 +1452,7 @@ public class WebOfTrust implements FredPlugin, FredPluginThreadless, FredPluginF
 	 * @throws UnknownIdentityException if there is no identity with this id in the database
 	 */
 	public Identity getIdentityByURI(FreenetURI uri) throws UnknownIdentityException {
-		return getIdentityByID(Identity.getIDFromURI(uri));
+		return getIdentityByID(IdentityID.getIDFromURI(uri));
 	}
 
 	/**
@@ -1475,7 +1476,7 @@ public class WebOfTrust implements FredPlugin, FredPluginThreadless, FredPluginF
 	 * @throws UnknownIdentityException if the OwnIdentity isn't in the database
 	 */
 	public OwnIdentity getOwnIdentityByURI(FreenetURI uri) throws UnknownIdentityException {
-		return getOwnIdentityByID(OwnIdentity.getIDFromURI(uri));
+		return getOwnIdentityByID(IdentityID.getIDFromURI(uri));
 	}
 
 	/**

src/plugins/WebOfTrust/ui/web/KnownIdentitiesPage.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.TreeMap;
 
 import plugins.WebOfTrust.Identity;
+import plugins.WebOfTrust.Identity.IdentityID;
 import plugins.WebOfTrust.OwnIdentity;
 import plugins.WebOfTrust.Score;
 import plugins.WebOfTrust.Trust;
@@ -83,7 +84,7 @@ public class KnownIdentitiesPage extends WebPageImpl {
 
 				try { 
 					if(addIdentity) { // Add a single identity and set its trust value
-						trusteeID = Identity.getIDFromURI(new FreenetURI(request.getPartAsStringFailsafe("IdentityURI", 1024)));
+						trusteeID = IdentityID.getIDFromURI(new FreenetURI(request.getPartAsStringFailsafe("IdentityURI", 1024)));
 						value = request.getPartAsStringFailsafe("Value", 4).trim();
 						comment = request.getPartAsStringFailsafe("Comment", Trust.MAX_TRUST_COMMENT_LENGTH + 1);				 	
 					} else { // Change multiple trust values via the known-identities-list

test/plugins/WebOfTrust/IdentityTest.java
@@ -6,6 +6,7 @@ package plugins.WebOfTrust;
 import java.net.MalformedURLException;
 
 import plugins.WebOfTrust.Identity.FetchState;
+import plugins.WebOfTrust.Identity.IdentityID;
 import plugins.WebOfTrust.exceptions.InvalidParameterException;
 import plugins.WebOfTrust.exceptions.UnknownIdentityException;
 
@@ -151,7 +152,7 @@ public final class IdentityTest extends DatabaseBasedTest {
 	}
 
 	public final void testGetIDFromURI() {
-		assertEquals(Base64.encode(uri.getRoutingKey()), Identity.getIDFromURI(uri));
+		assertEquals(Base64.encode(uri.getRoutingKey()), IdentityID.getIDFromURI(uri));
 	}
 
 	public final void testGetRequestURI() throws InvalidParameterException, MalformedURLException {

commit 7acf37ac833ea066aa75d51eaeb2023caf065f7a

Fix breakage introduced by commit 5685ed39131c19d697ec3430704c139c67f0a861

src/plugins/WebOfTrust/Identity.java
@@ -932,7 +932,7 @@ public class Identity extends Persistent implements Cloneable {
 		if(mRequestURI == null)
 			throw new NullPointerException("mRequestURI==null");
 		
-		if(!mID.equals(IdentityID.constructAndValidateFromURI(mRequestURI)))
+		if(!mID.equals(IdentityID.constructAndValidateFromURI(mRequestURI).toString()))
 			throw new IllegalStateException("ID does not match request URI!");
 		
 		IdentityID.constructAndValidateFromString(mID); // Throws if invalid