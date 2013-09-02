c871eb8ffe0d161604e2dbf95060cae340069d46
xor-freenet <xor@freenetproject.org>
2013-09-03 00:55:26 +0200

    Fix synchronization requirements JavaDoc of removeTrustWithoutCommit(Trust trust): - It is a caller of updateScoresWithoutCommit() so it has to constrain to its requirements. - I will update all callers of this function in the following commits.

--- src/plugins/WebOfTrust/WebOfTrust.java
+++ src/plugins/WebOfTrust/WebOfTrust.java
@@ -2290,10 +2290,11 @@ public class WebOfTrust implements FredPlugin, FredPluginThreadless, FredPluginF
 	 * This function does neither lock the database nor commit the transaction. You have to surround it with
 	 * synchronized(this) {
 	 * synchronized(mFetcher) {
+	 * synchronized(mSubscriptionManager) {
 	 * synchronized(Persistent.transactionLock(mDB)) {
 	 *     try { ... setTrustWithoutCommit(...); Persistent.checkedCommit(mDB, this); }
 	 *     catch(RuntimeException e) { Persistent.checkedRollbackAndThrow(mDB, this, e); }
-	 * }}}
+	 * }}}}
 	 * 
 	 */
 	protected void removeTrustWithoutCommit(Trust trust) {
