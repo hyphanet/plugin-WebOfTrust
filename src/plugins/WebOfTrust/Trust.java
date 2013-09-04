d59c283d4b658bed93e9d8d7218c87a9d1e5d317
xor-freenet <xor@freenetproject.org>
2013-09-04 03:52:22 +0200

    Fix Trust.clone() to clone the truster/trustee: - I've checked all callers for safety, one required modification - this commit also contains that fix. - This fixes part of the failure of the unit tests which were introduced in the previous unit tests.

--- src/plugins/WebOfTrust/Trust.java
+++ src/plugins/WebOfTrust/Trust.java
@@ -362,7 +362,7 @@ public final class Trust extends Persistent implements Cloneable {
 	
 	public Trust clone() {
 		try {
-			Trust clone = new Trust(mWebOfTrust, getTruster(), getTrustee(), getValue(), getComment());
+			Trust clone = new Trust(mWebOfTrust, getTruster().clone(), getTrustee().clone(), getValue(), getComment());
 			clone.mTrusterTrustListEdition = getTrusterEdition();
 			return clone;
 		} catch (InvalidParameterException e) {
