da1a75d675ed10ab899923ae1cfec6c3c0e895fc
xor-freenet <xor@freenetproject.org>
2013-09-08 02:49:55 +0200

    Document & complete session management code of WebInterfaceToadlet: - We did only provide code for peeking whether a session exists, not for using it => This commit adds a function for using it and obtaining the user ID. - The following commits will wire in that function to the rest of the web interface

--- src/plugins/WebOfTrust/ui/web/WebInterfaceToadlet.java
+++ src/plugins/WebOfTrust/ui/web/WebInterfaceToadlet.java
@@ -7,6 +7,7 @@ import java.util.NoSuchElementException;
 
 import javax.naming.SizeLimitExceededException;
 
+import plugins.WebOfTrust.OwnIdentity;
 import plugins.WebOfTrust.WebOfTrust;
 import plugins.WebOfTrust.exceptions.UnknownIdentityException;
 import plugins.WebOfTrust.ui.web.WebInterface.LogOutWebInterfaceToadlet;
@@ -15,10 +16,12 @@ import freenet.client.HighLevelSimpleClient;
 import freenet.clients.http.LinkEnabledCallback;
 import freenet.clients.http.RedirectException;
 import freenet.clients.http.SessionManager;
+import freenet.clients.http.SessionManager.Session;
 import freenet.clients.http.Toadlet;
 import freenet.clients.http.ToadletContext;
 import freenet.clients.http.ToadletContextClosedException;
 import freenet.node.NodeClientCore;
+import freenet.support.Logger;
 import freenet.support.api.HTTPRequest;
 
 public abstract class WebInterfaceToadlet extends Toadlet implements LinkEnabledCallback {
@@ -38,9 +41,48 @@ public abstract class WebInterfaceToadlet extends Toadlet implements LinkEnabled
 
 	abstract WebPage makeWebPage(HTTPRequest req, ToadletContext context) throws UnknownIdentityException, RedirectException;
 	
+	/**
+	 * Decides whether this Toadlet may be accessed. The GET/POST handler will only render the page if this returns true.
+	 * 
+	 * Checks whether a {@link SessionManager.Session} exists WITHOUT refreshing its timeout:
+	 * Besides denying access if a user tries to visit a Toadlet without a session, this is typically used for deciding which 
+	 * entries to display on the WOT menu on the web interface.
+	 * If a user is logged in, the menu should show everything which being logged in allows the user to use.
+	 * If no user is logged in, only the controls for allowing log in or creation of an account should be visible. 
+	 * Those checks should not mark the session as active since the user didn't actually do anything on the WOT web interface,
+	 * he was only active somewhere else on the Freenet web interface. Therefore, we don't refresh the session timeout.
+	 */
 	@Override
 	public boolean isEnabled(ToadletContext ctx) {
 		return sessionManager.sessionExists(ctx);
+	}
+	
+	/**
+	 * Check whether a {@link SessionManager.Session} exists AND refreshes its timeout. Returns the ID of the logged in user.
+	 * 
+	 * This is the front-end function for using session management in WebPage implementations.
+	 * 
+	 * ATTENTION: Throws a RedirectException to the {@link LoginWebInterfaceToadlet} if the session is expired. Therefore you must
+	 * not use this function in the code which generates the {@link LoginWebInterfaceToadlet} to prevent an infinite loop. 
+	 * 
+	 * @throws RedirectException If the session is not valid anymore. The redirect will be to the {@link LoginWebInterfaceToadlet}.
+	 * @return The ID of the logged in user. Typically the ID of the {@link OwnIdentity} which is logged in.
+	 */
+	public final String getLoggedInUserID(ToadletContext context) throws RedirectException {
+		final Session session = sessionManager.useSession(context);
+
+		if(session == null) {
+			// To prevent an infinite redirect loop from LogInWebInterfaceToadlet to itself, we check whether we ARE that toadlet.
+			// Its unlikely that this happens because this function is not be used in the code which handles requests to the 
+			// LoginWebInterfaceToadlet and the JavaDoc forbids using it for that as well.
+			// To guard against someone accidentally using it in the LogInWebInterfaceToadlet, we check nevertheless.
+			if(this instanceof LoginWebInterfaceToadlet)
+				throw new UnsupportedOperationException("You must not use this function in LogInWebInterfaceToadlet!");
+			
+			throw new RedirectException(webInterface.getToadlet(LoginWebInterfaceToadlet.class).getURI());
+		}
+
+		return session.getUserID();
 	}
 	
 	@Override
@@ -49,7 +91,7 @@ public abstract class WebInterfaceToadlet extends Toadlet implements LinkEnabled
 	}
 	
 	/**
-	 * Returns true if {@link #isEnabled(ToadletContext)}isEnabled() returns true. You must only proceed to handle GET/POST if this returrns true.
+	 * Returns true if {@link #isEnabled(ToadletContext)} returns true. You must only proceed to handle GET/POST if this returns true.
 	 * 
 	 * This function will try to redirect the user to the {@link LoginWebInterfaceToadlet} if that Toadlet is enabled and if its not equal to this Toadlet.
 	 * If redirecting to log in is not possible, it will attempt to redirect to {@link LogOutWebInterfaceToadlet}} with the same checks.
@@ -64,7 +106,7 @@ public abstract class WebInterfaceToadlet extends Toadlet implements LinkEnabled
 		
     	// If the current Toadlet is not enabled the likely reason is that nobody is logged in.
     	// So we want to redirect the user to the LoginWebInterfaceToadlet
-    	// However, we must not redirect to log in if we ARE the log in toaddlet to prevent a 100% CPU redirect loop.
+    	// However, we must not redirect to log in if we ARE the log in toadlet to prevent a 100% CPU redirect loop.
     	final WebInterfaceToadlet logIn = webInterface.getToadlet(LoginWebInterfaceToadlet.class);
     	if(this != logIn && logIn.isEnabled(ctx))
     		throw new RedirectException(logIn.getURI());
