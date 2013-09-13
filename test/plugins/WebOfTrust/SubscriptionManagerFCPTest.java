7d2e25904fecff8cf81037e55c3265b8ed47d35b
xor-freenet <xor@freenetproject.org>
2013-09-13 05:00:32 +0200

    Add ultra abstract framework for doing randomized tests of SubscriptionManager FCP: - I tried to shoot with all guns which Java offers so the code amount duplication is very little. I am rather proud of this :)

--- test/plugins/WebOfTrust/SubscriptionManagerFCPTest.java
+++ test/plugins/WebOfTrust/SubscriptionManagerFCPTest.java
@@ -3,13 +3,18 @@
  * any later version). See http://www.gnu.org/ for details of the GPL. */
 package plugins.WebOfTrust;
 
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import org.junit.Ignore;
 
+import plugins.WebOfTrust.exceptions.InvalidParameterException;
 import plugins.WebOfTrust.ui.fcp.FCPInterface;
 import freenet.node.FSParseException;
 import freenet.pluginmanager.PluginNotFoundException;
@@ -64,6 +69,30 @@ public class SubscriptionManagerFCPTest extends DatabaseBasedTest {
 		mFCPInterface.handle(mReplyReceiver, params, null, 0);
 	}
 
+	/**
+	 * The set of all {@link Identity}s as we have received them from the {@link SubscriptionManager.IdentitiesSubscription}.
+	 * Is compared to the actual contents of the {@link WebOfTrust} database at the end of the test.
+	 * 
+	 * Key = {@link Identity#getID()} 
+	 */
+	HashMap<String, Identity> mReceivedIdentities;
+	
+	/**
+	 * The set of all {@link Trust}s as we have received them from the {@link SubscriptionManager.TrustsSubscription}.
+	 * Is compared to the actual contents of the {@link WebOfTrust} database at the end of the test.
+	 * 
+	 * Key = {@link Trust#getID()} 
+	 */
+	HashMap<String, Trust> mReceivedTrusts;
+	
+	/**
+	 * The set of all {@link Score}s as we have received them from the {@link SubscriptionManager.ScoresSubscription}.
+	 * Is compared to the actual contents of the {@link WebOfTrust} database at the end of the test.
+	 * 
+	 * Key = {@link Score#getID()} 
+	 */
+	HashMap<String, Score> mReceivedScores;
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
@@ -113,4 +142,127 @@ public class SubscriptionManagerFCPTest extends DatabaseBasedTest {
 
 		assertFalse(mReplyReceiver.hasNextResult());
 	}
+	
+	
+	public void testAllRandomized() throws MalformedURLException, InvalidParameterException, FSParseException {
+		final int ownIdentityCount = 10;
+		final int identityCount = 100;
+		final int trustCount = (identityCount*identityCount) / 5; // A complete graph would be identityCount² trust values.
+
+		// Random trust graph setup...
+	
+		final ArrayList<Identity> identities = addRandomIdentities(identityCount / 10);
+		
+		// At least one own identity needs to exist to ensure that scores are computed.
+		for(int i=0; i < ownIdentityCount / 3; ++i) {
+			final OwnIdentity ownIdentity = mWoT.createOwnIdentity(getRandomSSKPair()[0], getRandomLatinString(Identity.MAX_NICKNAME_LENGTH), true, "Test");
+			identities.add(ownIdentity); 
+		}
+		
+		addRandomTrustValues(identities, trustCount);
+
+		/* Initial test data is set up */
+		subscribeAndSynchronize("Identities");
+		subscribeAndSynchronize("Trusts");
+		subscribeAndSynchronize("Scores");
+		
+		assertEquals(mReceivedIdentities, new HashSet<Identity>(mWoT.getAllIdentities()));
+		assertEquals(mReceivedTrusts, new HashSet<Trust>(mWoT.getAllTrusts()));
+		assertEquals(mReceivedScores, new HashSet<Score>(mWoT.getAllScores()));
+		
+		/* FIXME: Actually test event notifications by doing random changes of the WOT now */
+	}
+	
+	void subscribeAndSynchronize(final String type) throws FSParseException {
+		final SimpleFieldSet sfs = new SimpleFieldSet(true);
+		sfs.putOverwrite("Message", "Subscribe");
+		sfs.putOverwrite("To", type);
+		fcpCall(sfs);
+		
+		// First reply message is the full set of all objects of the type we are interested in so the client can synchronize its database
+		importSynchronization(type, mReplyReceiver.getNextResult());
+		
+		// Second reply message is the confirmation of the subscription
+		assertEquals("Subscribed", mReplyReceiver.getNextResult().get("Message"));
+		assertFalse(mReplyReceiver.hasNextResult());
+	}
+	
+	void importSynchronization(final String type, SimpleFieldSet synchronization) throws FSParseException {
+		/**
+		 * It is necessary to have this class instead of the following hypothetical function:
+		 * <code>void putAllWithDupecheck(final List<Persistent> source, final HashMap<String, Persistent> target)</code>
+		 * 
+		 * We couldn't shove a HashMap<String, Identity> into target of the hypothetical function even though Identity is a subclass of Persistent:
+		 * The reason is that GenericClass<Type> is not a superclass of GenericClass<subtype of Object> in Java:
+		 * "Parameterized types are not covariant."
+		 */
+		class ReceivedObjectPutter<T extends Persistent> {
+			
+			void putAllWithDupecheck(final List<T> source, final HashMap<String, T> target) {
+				for(final T p : source) {
+					assertFalse(target.containsKey(p.getID()));
+					target.put(p.getID(), p);
+				}
+			}
+		
+		}
+		
+		assertEquals(type, synchronization.get("Message"));
+		if(type.equals("Identities")) {
+			new ReceivedObjectPutter<Identity>().putAllWithDupecheck(new IdentityParser().parseMultiple(synchronization), mReceivedIdentities);
+		} else if(type.equals("Trusts")) {
+			new ReceivedObjectPutter<Trust>().putAllWithDupecheck(new TrustParser().parseMultiple(synchronization), mReceivedTrusts);
+		} else if(type.equals("Scores")) {
+			new ReceivedObjectPutter<Score>().putAllWithDupecheck(new ScoreParser().parseMultiple(synchronization), mReceivedScores);
+		}
+	}
+	
+
+	static abstract class FCPParser<T extends Persistent> {
+		
+		public ArrayList<T> parseMultiple(final SimpleFieldSet sfs) throws FSParseException {
+			final int amount = sfs.getInt("Amount");
+			final ArrayList<T> result = new ArrayList<T>(amount+1);
+			for(int i=0; i < amount; ++i) {
+				result.add(parseSingle(sfs, i));
+			}
+			return result;
+		}
+		
+		abstract protected T parseSingle(SimpleFieldSet sfs, int index);
+	
+	}
+	
+	static final class IdentityParser extends FCPParser<Identity> {
+
+		@Override
+		protected Identity parseSingle(final SimpleFieldSet sfs, final int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+	
+	static final class TrustParser extends FCPParser<Trust> {
+
+		@Override
+		protected Trust parseSingle(final SimpleFieldSet sfs, final int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+	static final class ScoreParser extends FCPParser<Score> {
+
+		@Override
+		protected Score parseSingle(final SimpleFieldSet sfs, final int i) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
+
 }
