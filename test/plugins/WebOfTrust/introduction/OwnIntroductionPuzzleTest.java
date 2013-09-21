6f377220f8a5236cf194cf4be37c3a850dd6ca1e
xor-freenet <xor@freenetproject.org>
2013-09-21 05:59:48 +0200

    Fix type to be OwnIntroductionPuzzle, not IntroductionPuzzle

--- test/plugins/WebOfTrust/introduction/OwnIntroductionPuzzleTest.java
+++ test/plugins/WebOfTrust/introduction/OwnIntroductionPuzzleTest.java
@@ -6,20 +6,13 @@ package plugins.WebOfTrust.introduction;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import plugins.WebOfTrust.DatabaseBasedTest;
-import plugins.WebOfTrust.OwnIdentity;
 import plugins.WebOfTrust.Persistent;
 import plugins.WebOfTrust.exceptions.InvalidParameterException;
-import plugins.WebOfTrust.introduction.IntroductionPuzzle.PuzzleType;
 import plugins.WebOfTrust.introduction.captcha.CaptchaFactory1;
 import freenet.support.CurrentTimeUTC;
 
@@ -41,13 +34,13 @@ public final class OwnIntroductionPuzzleTest extends DatabaseBasedTest {
 	}
 	
 	
-	protected IntroductionPuzzle constructPuzzle() throws MalformedURLException, IOException, InvalidParameterException {
+	protected OwnIntroductionPuzzle constructPuzzle() throws MalformedURLException, IOException, InvalidParameterException {
 		return mPuzzleFactory.generatePuzzle(mPuzzleStore, addRandomOwnIdentities(1).get(0));
 	}
 
 	@Test
 	public void testClone() throws MalformedURLException, IOException, InvalidParameterException, InterruptedException, IllegalArgumentException, IllegalAccessException {
-		final IntroductionPuzzle original = constructPuzzle();
+		final OwnIntroductionPuzzle original = constructPuzzle();
 		
 		Thread.sleep(10); // Persistent contains Date mCreationDate which might not get properly cloned.
 		assertFalse(CurrentTimeUTC.get().equals(original.getCreationDate()));
