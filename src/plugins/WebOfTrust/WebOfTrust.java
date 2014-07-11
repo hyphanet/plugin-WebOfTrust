hyphanet/plugin-WebOfTrust

commit 5e5f4b6cca5ea85996da88187f88a3758463f39c

Add restoreOwnIdentityWithoutCommit() and use it in the database upgrade code: - Previously restoreOwnIdentity() did its own transaction. This meant that the database upgrade transaction could not be fully rolled back in all cases.

src/plugins/WebOfTrust/WebOfTrust.java
@@ -617,9 +617,9 @@ public final class WebOfTrust extends WebOfTrustInterface implements FredPlugin,
 			Logger.normal(this, "Upgrading database version " + databaseVersion);
 			
 			//synchronized(this) { // Already done at function level
-			synchronized(mPuzzleStore) { // For deleteWithoutCommit(Identity) / restoreOwnIdentity()
-			synchronized(mFetcher) { // For deleteWithoutCommit(Identity) / restoreOwnIdentity()
-			synchronized(mSubscriptionManager) { // For deleteWithoutCommit(Identity) / restoreOwnIdentity()
+			synchronized(mPuzzleStore) { // For deleteWithoutCommit(Identity) / restoreOwnIdentityWithoutCommit()
+			synchronized(mFetcher) { // For deleteWithoutCommit(Identity) / restoreOwnIdentityWithoutCommit()
+			synchronized(mSubscriptionManager) { // For deleteWithoutCommit(Identity) / restoreOwnIdentityWithoutCommit()
 				synchronized(Persistent.transactionLock(mDB)) {
 					try {
 						Logger.normal(this, "Searching for duplicate OwnIdentity objects...");
@@ -655,7 +655,7 @@ public final class WebOfTrust extends WebOfTrustInterface implements FredPlugin,
 						
 							Logger.warning(this, "Restoring a single OwnIdentity for the deleted duplicates...");
 							try {
-								restoreOwnIdentity(insertURI);
+								restoreOwnIdentityWithoutCommit(insertURI);
 							} catch (Exception e) {
 								throw new RuntimeException(e);
 							}
@@ -3150,16 +3150,24 @@ public final class WebOfTrust extends WebOfTrustInterface implements FredPlugin,
 
 	/**
 	 * NOTICE: When changing this function, please also take care of {@link OwnIdentity.isRestoreInProgress()}
+	 * 
+	 * This function does neither lock the database nor commit the transaction. You have to surround it with
+	 * <code>
+	 * synchronized(WebOfTrust.this) {
+	 * synchronized(mPuzzleStore) {
+	 * synchronized(mFetcher) {
+	 * synchronized(mSubscriptionManager) {
+	 * synchronized(Persistent.transactionLock(mDB)) {
+	 *     try { ... restoreOwnIdentityWithoutCommit(...); Persistent.checkedCommit(mDB, this); }
+	 *     catch(RuntimeException e) { Persistent.checkedRollbackAndThrow(mDB, this, e); }
+	 * }}}}
+	 * </code>
 	 */
-	public synchronized void restoreOwnIdentity(FreenetURI insertFreenetURI) throws MalformedURLException, InvalidParameterException {
+	public void restoreOwnIdentityWithoutCommit(FreenetURI insertFreenetURI) throws MalformedURLException, InvalidParameterException {
 		Logger.normal(this, "restoreOwnIdentity(): Starting... ");
 		
 		OwnIdentity identity;
-		
-		synchronized(mPuzzleStore) {
-		synchronized(mFetcher) {
-		synchronized(mSubscriptionManager) {
-		synchronized(Persistent.transactionLock(mDB)) {
+
 			try {
 				long edition = 0;
 				
@@ -3292,23 +3300,37 @@ public final class WebOfTrust extends WebOfTrustInterface implements FredPlugin,
 				// This function messes with the trust graph manually so it is a good idea to check whether it is intact afterwards.
 				assert(computeAllScoresWithoutCommit());
 				
-				Persistent.checkedCommit(mDB, this);
+				Logger.normal(this, "restoreOwnIdentity(): Finished.");
 			}
 			catch(RuntimeException e) {
 				if(mTrustListImportInProgress) { // We don't execute beginTrustListImport() in all code paths of this function
-					abortTrustListImport(e); // Does rollback for us
+					// Does rollback for us. The outside will do another duplicate rollback() because the JavaDoc tells it to.
+					// But thats acceptable to keep the transaction code pattern the same everywhere.
+					abortTrustListImport(e); 
 					throw e;
-				} else {
-					Persistent.checkedRollbackAndThrow(mDB, this, e);
 				}
+			}
+
+	}
+	
+	public synchronized void restoreOwnIdentity(FreenetURI insertFreenetURI) throws MalformedURLException, InvalidParameterException {
+		synchronized(mPuzzleStore) {
+		synchronized(mFetcher) {
+		synchronized(mSubscriptionManager) {
+		synchronized(Persistent.transactionLock(mDB)) {
+			try {
+				restoreOwnIdentityWithoutCommit(insertFreenetURI);
+				Persistent.checkedCommit(mDB, this);
+			}
+			catch(RuntimeException e) {
+				Persistent.checkedRollbackAndThrow(mDB, this, e);
 			}
 		}
 		}
 		}
 		}
-		
-		Logger.normal(this, "restoreOwnIdentity(): Finished.");
 	}
+
 
 	public synchronized void setTrust(String ownTrusterID, String trusteeID, byte value, String comment)
 		throws UnknownIdentityException, NumberFormatException, InvalidParameterException {

commit ace7004dd4099f3451ea3e78e0f11a35b9f76fd1

Fix restoreOwnIdentityWithoutCommit() swallowing its own Exceptions: - This could have caused transactions to not be rolled back, which is *bad* - Luckily, the code was not deployed in a build yet, so we don't need to introduce database upgrade code to deal with possible sideeffects. The issue was introduced by: https://github.com/freenet/plugin-WoT-staging/commit/5e5f4b6cca5ea85996da88187f88a3758463f39c

src/plugins/WebOfTrust/WebOfTrust.java
@@ -3314,8 +3314,9 @@ public final class WebOfTrust extends WebOfTrustInterface implements FredPlugin,
 					// Does rollback for us. The outside will do another duplicate rollback() because the JavaDoc tells it to.
 					// But thats acceptable to keep the transaction code pattern the same everywhere.
 					abortTrustListImport(e); 
-					throw e;
 				}
+				// The callers of this function are obliged to do Persistent.checkedRollbackAndThrow() for us, so we can and must throw the exception out.
+				throw e;
 			}
 
 	}