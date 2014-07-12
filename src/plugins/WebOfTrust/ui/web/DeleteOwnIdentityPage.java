ac95f5566dee13cfe0fd946f3163850ce9ba4218
xor-freenet <xor@freenetproject.org>
2014-07-12 09:55:00 +0200

    DeleteOwnIdentityPage: When asking the user for confirmation, display the unique nickname, not just the nickname: - Users might have multiple own identities with the same nickname, so displaying the unique version (= contains part of the unique identity ID) prevents mixup.

--- src/plugins/WebOfTrust/ui/web/DeleteOwnIdentityPage.java
+++ src/plugins/WebOfTrust/ui/web/DeleteOwnIdentityPage.java
@@ -59,7 +59,7 @@ public class DeleteOwnIdentityPage extends WebPageImpl {
 	private void makeConfirmation() {
 		HTMLNode box = addContentBox(l10n().getString("DeleteOwnIdentityPage.DeleteIdentityBox.Header"));
 
-		box.addChild(new HTMLNode("p", l10n().getString("DeleteOwnIdentityPage.DeleteIdentityBox.Text1", "nickname", mIdentity.getNickname())));
+		box.addChild(new HTMLNode("p", l10n().getString("DeleteOwnIdentityPage.DeleteIdentityBox.Text1", "nickname", mIdentity.getShortestUniqueNickname())));
 		box.addChild(new HTMLNode("p", l10n().getString("DeleteOwnIdentityPage.DeleteIdentityBox.Text2")));
 
 		HTMLNode confirmForm = pr.addFormChild(box, uri.toString(), "DeleteIdentity");
