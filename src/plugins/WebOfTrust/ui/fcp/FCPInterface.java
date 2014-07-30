de98787189acbd8d22472fd3d2a4e94d773bd7bb
xor-freenet <xor@freenetproject.org>
2014-07-30 23:02:19 +0200

    FCPInterface.ClientTrackerDaemon: Fix typo in logging

--- src/plugins/WebOfTrust/ui/fcp/FCPInterface.java
+++ src/plugins/WebOfTrust/ui/fcp/FCPInterface.java
@@ -185,7 +185,7 @@ public final class FCPInterface implements FredPluginFCP {
 	        			final WeakReference<PluginReplySender> removedClient = mClientsByID.remove(removedID);
 	        			assert(removedID != null);
 	        			assert(removedClient != null);
-	        			Logger.normal(this, "Garbe-collecting disconnected client: remaining clients = " + mClientsByID.size() + "; client ID = " + removedID);
+	        			Logger.normal(this, "Garbage-collecting disconnected client: remaining clients = " + mClientsByID.size() + "; client ID = " + removedID);
 	        		}
 	        	} catch(InterruptedException e) {
 	        		Thread.interrupted();
