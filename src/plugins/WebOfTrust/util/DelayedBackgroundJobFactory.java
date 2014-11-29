e823b10f3d4da92eca0033d0153e6ecfd52b9386
Bert Massop <bert.massop@gmail.com>
2014-11-29 01:14:30 +0100

    DelayedBackgroundJob: improve factory termination helpers

--- src/plugins/WebOfTrust/util/DelayedBackgroundJobFactory.java
+++ src/plugins/WebOfTrust/util/DelayedBackgroundJobFactory.java
@@ -1,5 +1,6 @@
 package plugins.WebOfTrust.util;
 
+import java.util.ArrayList;
 import java.util.WeakHashMap;
 
 import freenet.support.Executor;
@@ -73,7 +74,48 @@ public class DelayedBackgroundJobFactory {
             for (BackgroundJob bg : aliveJobSet.keySet()) {
                 bg.terminate();
             }
-            aliveJobSet.clear();
+        }
+    }
+
+    /**
+     * Checks whether all live {@link BackgroundJob BackgroundJobs} created by this factory are
+     * terminated.
+     * @return {@code true} if all jobs are terminated.
+     */
+    public boolean allTerminated() {
+        boolean allTerminated = true;
+        synchronized(aliveJobSet) {
+            for (BackgroundJob bg : aliveJobSet.keySet()) {
+                allTerminated &= bg.isTerminated();
+            }
+        }
+        return allTerminated;
+    }
+
+    /**
+     * Waits for all live {@link BackgroundJob BackgroundJobs} created by this factory to
+     * terminate. Note that this method makes a snapshot of the currently live jobs, so jobs
+     * created after the wait has started will not be waited upon. This method returns when all
+     * jobs in this snapshot have terminated, if the timeout has expired, or if the wait is
+     * interrupted.
+     * This method does not terminate the jobs, it only waits for them to be terminated.
+     * @param timeout the maximum time to wait
+     * @throws InterruptedException If the wait is interrupted.
+     * @see BackgroundJob#waitForTermination(long)
+     * @see #terminateAll()
+     */
+    public void waitForTerminationOfAll(long timeout) throws InterruptedException {
+        ArrayList<BackgroundJob> jobs;
+        synchronized(aliveJobSet) {
+            jobs = new ArrayList<BackgroundJob>(aliveJobSet.keySet());
+        }
+        long deadline = System.currentTimeMillis() + timeout;
+        for (BackgroundJob job : jobs) {
+            job.waitForTermination(timeout);
+            timeout = deadline - System.currentTimeMillis();
+            if (timeout <= 0) {
+                return;
+            }
         }
     }
 }
