1991fc46bac0c6d4615d9b1de140b337757116a0
xor-freenet <xor@freenetproject.org>
2014-12-17 23:41:19 +0100

    FCPInterface.handleGetScores(): Fix JavaDoc copypaste glitch

--- src/plugins/WebOfTrust/ui/fcp/FCPInterface.java
+++ src/plugins/WebOfTrust/ui/fcp/FCPInterface.java
@@ -783,7 +783,7 @@ public final class FCPInterface implements FredPluginFCPMessageHandler.ServerSid
     
     /**
      * @param request
-     *            Can be null if you use this to send out Trusts due to an event, not due to
+     *            Can be null if you use this to send out Scores due to an event, not due to
      *            an original client message.
      */
     private FCPPluginMessage handleGetScores(final FCPPluginMessage request) {
