02174a2d43fcedbb2ddabe06dce37f5d5ec22f02
xor-freenet <xor@freenetproject.org>
2014-12-19 21:09:35 +0100

    FCPInterface.handleGetTrustersCount(): Document why we synchronize
    This commit would have added synchronization similar to the preceding
    commits, but the code already contains the necessary synchronization.
    Thus, only add a comment which explains why it is there.

--- src/plugins/WebOfTrust/ui/fcp/FCPInterface.java
+++ src/plugins/WebOfTrust/ui/fcp/FCPInterface.java
@@ -936,10 +936,18 @@ public final class FCPInterface implements FredPluginFCPMessageHandler.ServerSid
     		else if (selection.equals("0")) select = 0;
     		else throw new InvalidParameterException("Unhandled selection value (" + selection + ")");
         	
+            // getReceivedTrusts() demands that we synchronize while processing the result.
+            // Also, we query the Identity truster before calling it, i.e. query two datasets
+            // from the database. Thus we must synchronize to ensure that the returned data is
+            // coherent - the truster might be deleted meanwhile.
     		synchronized(mWoT) {
         		result = mWoT.getReceivedTrusts(mWoT.getIdentityByID(identityID), select).size();
         	}
         } else {
+            // getReceivedTrusts() demands that we synchronize while processing the result.
+            // Also, we query the Identity truster before calling it, i.e. query two datasets
+            // from the database. Thus we must synchronize to ensure that the returned data is
+            // coherent - the truster might be deleted meanwhile.
         	synchronized(mWoT) {
         		result = mWoT.getReceivedTrusts(mWoT.getIdentityByID(identityID)).size();
         	}
