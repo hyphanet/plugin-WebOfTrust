2c3df28f1b9e59f9fdda4064d207fb3fdb7b309b
xor-freenet <xor@freenetproject.org>
2015-02-14 01:25:37 +0100

    plugins.WebOftrust.util.jobs.*: Add GPL license header
    The author bertm explicitely allowed me to do so:
    https://github.com/freenet/plugin-WebOfTrust/pull/26#issuecomment-74251701

--- src/plugins/WebOfTrust/util/jobs/package-info.java
+++ src/plugins/WebOfTrust/util/jobs/package-info.java
@@ -1,3 +1,7 @@
+/* This code is part of WoT, a plugin for Freenet. It is distributed
+ * under the GNU General Public License, version 2 (or at your option
+ * any later version). See http://www.gnu.org/ for details of the GPL. */
+
 /**
  * Implementations of {@link plugins.WebOfTrust.util.jobs.BackgroundJob BackgroundJob}. The ideas
  * behind their purpose are the following:<br>
