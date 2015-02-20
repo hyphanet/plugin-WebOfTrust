816e685342dfb225875fc74a53349b7e97759a56
xor-freenet <xor@freenetproject.org>
2015-02-20 20:23:05 +0100

    SubscriptionManager.stop(): Remove resolved FIXME.
    I have tested this, it works, including interrupting the thread if it is
    currently running.

--- src/plugins/WebOfTrust/SubscriptionManager.java
+++ src/plugins/WebOfTrust/SubscriptionManager.java
@@ -1994,7 +1994,6 @@ public final class SubscriptionManager implements PrioRunnable {
 	
 	/**
 	 * Shuts down this SubscriptionManager by aborting all queued notification processing and waiting for running processing to finish.
-	 * FIXME: Test.<br><br>
 	 * 
 	 * Notice: Not synchronized so it can be run in parallel with {@link #run()}. This will allow it
 	 * to call {@link DelayedBackgroundJob#terminate()} while run() is executing, which calls
