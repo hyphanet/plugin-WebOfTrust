e987a071660e3eaaa60ab8a396d93ece732a5246
xor-freenet <xor@freenetproject.org>
2016-08-09 02:42:31 +0200

    New package: plugins.WebOfTrust.network.input

---
+++ src/plugins/WebOfTrust/network/input/package-info.java
@@ -0,0 +1,12 @@
+/* This code is part of WoT, a plugin for Freenet. It is distributed 
+ * under the GNU General Public License, version 2 (or at your option
+ * any later version). See http://www.gnu.org/ for details of the GPL. */
+
+
+/**
+ * Contains code to download {@link plugins.WebOfTrust.Identity}s and
+ * {@link plugins.WebOfTrust.Trust}s from the network.
+ * 
+ * Notice: {@link plugins.WebOfTrust.introduction.IntroductionPuzzle}s are handled by the packages
+ * plugins.WebOfTrust.introduction. **/
+package plugins.WebOfTrust.network.input;
