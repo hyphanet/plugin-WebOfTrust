74feb85e2579a67fcf47e3db8a81b0ad33ae926f
xor-freenet <xor@freenetproject.org>
2016-08-22 07:29:44 +0200

    ScoreTest: Finish test for constructor

--- test/plugins/WebOfTrust/ScoreTest.java
+++ test/plugins/WebOfTrust/ScoreTest.java
@@ -6,6 +6,7 @@ package plugins.WebOfTrust;
 import static org.junit.Assert.*;
 
 import java.net.MalformedURLException;
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -67,6 +68,61 @@ public class ScoreTest extends AbstractJUnit4BaseTest {
 		assertEquals(truster, s2.getTruster());
 		assertEquals(truster, s2.getTrustee());
 		s2 = null;
+		
+		// Test integer params
+		
+		// All integer values are allowed.
+		int[] goodValues = { Integer.MIN_VALUE, -2, -1, 0, 1, 2, Integer.MAX_VALUE };
+		// Anything >= 0 is allowed.
+		int[] goodRanks = { 0, 1, 2, Integer.MAX_VALUE  };
+		// Only those specific values are legal. Same as WebOfTrust.capacities.
+		// We intentionally don't use the same array object here to ensure people who change it will
+		// notice that this test fails and should be reviewed for whether the rest of it still makes
+		// sense with the new capacity values.
+		int[] goodCapacities = { 0, 1, 2, 6, 16, 40, 100 };
+
+		// Anything < 0 is illegal
+		int[] badRanks = new int[] { Integer.MIN_VALUE, -2, -1 } ;
+		// Anything not in goodCapacities is illegal
+		int[] badCapacities = new int[] { Integer.MIN_VALUE, -2, -1, 3, 7, 101, Integer.MAX_VALUE };
+		
+		// Score has 3 separate setters for rank, value and capacity. This means that as a design
+		// decision, it does not check whether the whole combination of value/rank/capacity does
+		// make sense from a Score computation point of view as specified by
+		// WebOfTrust.computeAllScoresWithoutCommit().
+		// Since the setters are also used in the constructor, we thus don't check for combinations
+		// which don't make sense.
+		// TODO: Code quality: Change Score to have a single setter with cross-checks to fix this:
+		//    set(int value, int rank, int capacity)
+		// Then add explicit tests for:
+		// - illegal combinations of legal value/rank/score
+		// - the capacity not matching the rank.
+		// Also, unfold the loop to not produce such combinations anymore.
+		for(int value : goodValues) {
+			for(int rank : goodRanks) {
+				for(int capacity : goodCapacities) {
+					Score s3 = new Score(wot, truster, trustee, value, rank, capacity);
+					assertEquals(value, s3.getScore());
+					assertEquals(rank, s3.getRank());
+					assertEquals(capacity, s3.getCapacity());
+					s3 = null;
+					
+					for(int badRank : badRanks) {
+						try {
+							new Score(wot, truster, trustee, value, badRank, capacity);
+							fail();
+						} catch(IllegalArgumentException e) {}
+					}
+					
+					for(int badCapacity : badCapacities) {
+						try {
+							new Score(wot, truster, trustee, value, rank, badCapacity);
+							fail();
+						} catch(IllegalArgumentException e) {}
+					}
+				}
+			}
+		}
 	}
 
 	@Override protected WebOfTrust getWebOfTrust() {
