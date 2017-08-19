hyphanet/plugin-WebOfTrust

commit 0976608e8c29a88022f613ce48fbfbdebaac5f75

IdentityDownloaderFast.storeStartFetchCommand...(): Fix to store a DownloadSchedulerCommand instead of invoking the DownloadScheduler directly
See the FIXME at class DownloadScheduler.
Further changes at other functions will be served by the upcoming
commits to fully implement that FIXME.

src/plugins/WebOfTrust/Persistent.java
@@ -410,7 +410,7 @@ public abstract class Persistent implements Serializable {
 	 * 	catch(RuntimeException e) { Persistent.checkedRollbackAndThrow(mDB, this, e); }<br />
 	 * } 
 	 */
-	protected void storeWithoutCommit() {
+	public void storeWithoutCommit() {
 		// TODO: Code quality: Use activateFully() and get rid of the depth parameter of the 
 		// function we call here, or at least make it private:
 		// The existence of the depth-consuming function makes child classes likely to use the
@@ -448,7 +448,7 @@ public abstract class Persistent implements Serializable {
 	 * 	catch(RuntimeException e) { Persistent.checkedRollbackAndThrow(mDB, this, e); }<br />
 	 * } 
 	 */
-	protected void deleteWithoutCommit() {
+	public void deleteWithoutCommit() {
 		// TODO: Code quality: Use activateFully() and get rid of the depth parameter of the 
 		// function we call here, or at least make it private:
 		// The existence of the depth-consuming function makes child classes likely to use the

src/plugins/WebOfTrust/network/input/IdentityDownloaderFast.java
@@ -222,8 +222,22 @@ public final class IdentityDownloaderFast implements
 		// While a call to this function means that any OwnIdentity wants it to be downloaded
 		// indeed we do *not* know whether that desire is due to a direct trust value from any
 		// OwnIdentity. Thus we need to check with shouldDownload().
-		if(shouldDownload(identity))
+		if(shouldDownload(identity)) {
+			DownloadSchedulerCommand c = getQueuedCommand(identity);
+			
+			if(c != null) {
+				if(c instanceof StartDownloadCommand)
+					return;
+				
+				if(c instanceof StopDownloadCommand)
+					c.deleteWithoutCommit();
+			}
+			
+			c = new StartDownloadCommand(mWoT, identity);
+			c.storeWithoutCommit();
+			
 			mDownloadSchedulerThread.triggerExecution();
+		}
 	}
 
 	@Override public void storeAbortFetchCommandWithoutCommit(Identity identity) {

commit c99f51b95f70b30a36d19f63a8aae797c4b30c0f

Persisent: Fix compiler errors caused by too high function visibilities
The compiler would complain that the implementing child classes all over
the WebOfTrust main package try to decrease the visiblity of the
inherited functions, which they are not entitled to do: Child classes
may only increase visibilities, not decrease them.

This was caused by commit 0976608e8c29a88022f613ce48fbfbdebaac5f75
which thus hereby is partly replaced with a better implementation of
making those functions visible for users of class
DownloadSchedulerCommand:
Instead of increasing the visiblity at the base class Persistent add
skeletons of the functions to class DownloadSchedulerCommand to
increase their visibility.
Keeping the visibilty of "protected" at class Persistent allows us
to prevent non-core code such as the UI from messing with database
functions.

src/plugins/WebOfTrust/Persistent.java
@@ -410,7 +410,7 @@ public abstract class Persistent implements Serializable {
 	 * 	catch(RuntimeException e) { Persistent.checkedRollbackAndThrow(mDB, this, e); }<br />
 	 * } 
 	 */
-	public void storeWithoutCommit() {
+	protected void storeWithoutCommit() {
 		// TODO: Code quality: Use activateFully() and get rid of the depth parameter of the 
 		// function we call here, or at least make it private:
 		// The existence of the depth-consuming function makes child classes likely to use the
@@ -448,7 +448,7 @@ public abstract class Persistent implements Serializable {
 	 * 	catch(RuntimeException e) { Persistent.checkedRollbackAndThrow(mDB, this, e); }<br />
 	 * } 
 	 */
-	public void deleteWithoutCommit() {
+	protected void deleteWithoutCommit() {
 		// TODO: Code quality: Use activateFully() and get rid of the depth parameter of the 
 		// function we call here, or at least make it private:
 		// The existence of the depth-consuming function makes child classes likely to use the

src/plugins/WebOfTrust/network/input/IdentityDownloaderFast.java
@@ -386,6 +386,16 @@ public final class IdentityDownloaderFast implements
 			     + "; mIdentity.getID(): " + getIdentity().getID()
 			     + "]";
 		}
+
+		/** Overriden for visibility only. */
+		@Override protected void storeWithoutCommit() {
+			super.storeWithoutCommit();
+		}
+
+		/** Overriden for visibility only. */
+		@Override protected void deleteWithoutCommit() {
+			super.deleteWithoutCommit();
+		}
 	}
 
 	@SuppressWarnings("serial")