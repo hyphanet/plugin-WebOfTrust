287d14777073a86420005f8475685312301500b8
xor-freenet <xor@freenetproject.org>
2017-10-06 21:28:50 +0200

    IntroductionClientTest: Fix puzzle up+download to take ~70 seconds instead of >= 10 minutes
    ... on my 2.4 GHz Core 2 Duo.
    
    For the reason see the first added comment.

--- test/plugins/WebOfTrust/introduction/IntroductionClientTest.java
+++ test/plugins/WebOfTrust/introduction/IntroductionClientTest.java
@@ -22,9 +22,6 @@ import plugins.WebOfTrust.exceptions.InvalidParameterException;
 import plugins.WebOfTrust.exceptions.UnknownIdentityException;
 import plugins.WebOfTrust.exceptions.UnknownPuzzleException;
 import plugins.WebOfTrust.util.StopWatch;
-
-import com.db4o.ObjectSet;
-
 import freenet.node.Node;
 
 /**
@@ -88,39 +85,26 @@ public final class IntroductionClientTest extends AbstractMultiNodeTest {
 		OwnIdentity serverIdentity;
 		OwnIdentity clientIdentity;
 		
+		// Create the server Identity which will upload a puzzle.
+		// Don't create the client Identity yet: That would result in the client trying to download
+		// puzzles immediately, which would fail because the puzzles of the server aren't uploaded
+		// yet, and the IntroductionClient puzzle download loop has a MINIMAL_SLEEP_TIME of 10
+		// minutes, so retrying once they are uploaded would take >= 10 minutes. 
+		//
 		// Synchronized to prevent the WoTs from doing stuff while we set up the test environment.
 		// synchronized & clone() also as workaround for
 		// https://bugs.freenetproject.org/view.php?id=6247
+		//
 		// Notice: As a consequence of the clone() we will have to re-query the identities from the
 		// database before we pass them to other functions which use them for database queries,
 		// otherwise db4o will not know the objects' references.
 		synchronized(serverWoT) {
-		synchronized(clientWoT) {
-		synchronized(clientStore) {
 			// Server Identity must have publishTrustList == true to enable puzzle publishing.
 			serverIdentity = serverWoT.createOwnIdentity("serverId", true, null).clone();
-			// Disable trust list and hence also puzzle upload for the client to reduce traffic.
-			clientIdentity = clientWoT.createOwnIdentity("clientId", false, null).clone();
 			
 			// Reduce amount of inserted puzzles to speed things up and keep our code simple.
 			serverWoT.setPublishIntroductionPuzzles(serverIdentity.getID(), true, 1);
-			
-			// Create the server identity at the client so the client knows where to download
-			// puzzles from.
-			clientWoT.createOwnIdentity(serverIdentity.getInsertURI(), "serverId", true, null);
-			clientWoT.setPublishIntroductionPuzzles(serverIdentity.getID(), true, 1);
-			// Convert it to a non-own Identity so we have to download the puzzles from the remote
-			// instance instead of just creating them locally.
-			clientWoT.deleteOwnIdentity(serverIdentity.getID());
-			Identity serverIdentityAtClient = clientWoT.getIdentityByID(serverIdentity.getID());
-			assertTrue(serverIdentityAtClient.hasContext(INTRODUCTION_CONTEXT));
-			assertEquals("1", serverIdentityAtClient.getProperty(PUZZLE_COUNT_PROPERTY));
-			assertEquals(0, clientStore.getNonOwnCaptchaAmount(false));
-			
-			// The client ID must trust the server ID so it will actually download the server ID and
-			// its puzzles.
-			clientWoT.setTrust(clientIdentity.getID(), serverIdentity.getID(), (byte)100, "");
-		}}}
+		}
 		
 		// Speed up generation / upload of the puzzle.
 		server.nextIteration();
@@ -145,41 +129,61 @@ public final class IntroductionClientTest extends AbstractMultiNodeTest {
 		} while(true);
 		System.out.println("IntroductionClientTest: Puzzle generated! Time: " + generationTime);
 		
-		System.out.println("IntroductionClientTest: Waiting for puzzle to be up-/downloaded...");
+		System.out.println("IntroductionClientTest: Waiting for puzzle to be uploaded...");
 		StopWatch uploadTime = new StopWatch();
-		StopWatch downloadTime = new StopWatch();
-		boolean uploaded = false;
-		boolean downloaded = false;
 		do {
 			// Check whether the IntroductionPuzzle was uploaded and show the time it took to do so.
-			// Notice: We intentionally don't wait for this in a separate loop before waiting for it
-			// to be downloaded: Due to redundancy the amount of data to upload is larger than what
-			// has to be downloaded, so fred's "upload finished!" callbacks can happen AFTER the
-			// remote node's "download finished!" callbacks have already returned.
-			if(!uploaded) {
-				synchronized(serverWoT) {
-				synchronized(serverStore) {
-					OwnIntroductionPuzzle p = (OwnIntroductionPuzzle)serverStore.getByID(puzzleID);
-					if(p.wasInserted()) {
-						uploaded = true;
-						System.out.println(
-							"IntroductionClientTest: Puzzle uploaded! Time: " + uploadTime);
-						
-						// Speed up download of the puzzle
-						client.nextIteration();
-					}
-				}}
-			}
+			synchronized(serverWoT) {
+			synchronized(serverStore) {
+				OwnIntroductionPuzzle p = (OwnIntroductionPuzzle)serverStore.getByID(puzzleID);
+				if(p.wasInserted())
+					break;
+			}}
 			
+			sleep(1000);
+		} while(true);
+		System.out.println("IntroductionClientTest: Puzzle uploaded! Time: " + uploadTime);
+		
+		// Create the client Identity which will download the puzzle.
+		synchronized(clientWoT) {
+		synchronized(clientStore) {
+			// Disable trust list and hence also puzzle upload for the client to reduce traffic.
+			clientIdentity = clientWoT.createOwnIdentity("clientId", false, null).clone();
+			
+			// Create the server identity at the client so the client knows where to download
+			// puzzles from.
+			clientWoT.createOwnIdentity(serverIdentity.getInsertURI(), "serverId", true, null);
+			clientWoT.setPublishIntroductionPuzzles(serverIdentity.getID(), true, 1);
+			// Convert it to a non-own Identity so we have to download the puzzles from the remote
+			// instance instead of just creating them locally.
+			clientWoT.deleteOwnIdentity(serverIdentity.getID());
+			Identity serverIdentityAtClient = clientWoT.getIdentityByID(serverIdentity.getID());
+			assertTrue(serverIdentityAtClient.hasContext(INTRODUCTION_CONTEXT));
+			assertEquals("1", serverIdentityAtClient.getProperty(PUZZLE_COUNT_PROPERTY));
+			assertEquals(0, clientStore.getNonOwnCaptchaAmount(false));
+			
+			// The client ID must trust the server ID so it will actually download the server ID and
+			// its puzzles.
+			clientWoT.setTrust(clientIdentity.getID(), serverIdentity.getID(), (byte)100, "");
+		}}
+		
+		// Not necessary: createOwnIdentity() does it.
+		/*
+			// Speed up generation / upload of the puzzle.
+			client.nextIteration();
+		*/
+		
+		System.out.println("IntroductionClientTest: Waiting for puzzle to downloaded...");
+		StopWatch downloadTime = new StopWatch();
+		do {
 			synchronized(clientWoT) {
 			synchronized(clientStore) {
 				if(clientStore.getNonOwnCaptchaAmount(false) == 1)
-					downloaded = true;
+					break;
 			}}
 			
-			if(!downloaded)
-				sleep(1000);
-		} while(!downloaded);
+			sleep(1000);
+		} while(true);
 		System.out.println("IntroductionClientTest: Puzzle downloaded! Time: " + downloadTime);
 		
 		System.out.println("IntroductionClientTest: testFullIntroductionCycle() done! Time: " + t);
