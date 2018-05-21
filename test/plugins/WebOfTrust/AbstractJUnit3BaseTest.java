0532d6df9206a2ccbf70d64a09edffa03a6e284d
xor-freenet <xor@freenetproject.org>
2018-05-21 05:10:55 +0200

    AbstractJUnit3BaseTest.testClone(): Indent

--- test/plugins/WebOfTrust/AbstractJUnit3BaseTest.java
+++ test/plugins/WebOfTrust/AbstractJUnit3BaseTest.java
@@ -135,9 +135,9 @@ public class AbstractJUnit3BaseTest extends TestCase {
 			// Check all fields for whether assertNotSame() applies to them, i.e. for whether the
 			// clone does not wrongly re-use objects of the original.
 			// Exclude fields for which using the same object would be safe.
-			if(!field.getType().isPrimitive() // int etc. aren't objects. See testIsPrimitive().
+			if(    !field.getType().isPrimitive() // int etc. aren't objects. See testIsPrimitive().
 				&& !field.getType().isEnum() // Enum objects exist only once
-				&& field.getType() != String.class // Strings are interned and therefore might also exist only once
+				&&  field.getType() != String.class // Strings are interned and therefore might also exist only once
 				&& !Modifier.isTransient(field.getModifiers()) // Persistent.mWebOfTurst/mDB are transient field which have the same value everywhere
 				&& !Modifier.isStatic(field.getModifiers()))
 			{
