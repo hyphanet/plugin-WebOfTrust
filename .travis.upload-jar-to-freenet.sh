47534245a4ff9ac3aa6ee415262ff415de369cb5
xor-freenet <xor@freenetproject.org>
2018-05-31 01:10:01 +0200

    Travis CI: Fix WoT JAR upload w/ pyFreenet by manually starting the node

--- .travis.upload-jar-to-freenet.sh
+++ .travis.upload-jar-to-freenet.sh
@@ -4,22 +4,68 @@ set -o errexit
 set -o errtrace
 trap 'echo "Error at line $LINENO, exit code $?" >&2' ERR
 
-echo "Installing pyFreenet3..."
-pip3 install -U --user --egg pyFreenet3
-
 FILENAME="WebOfTrust-$(git describe)-built-on-$TRAVIS_JDK_VERSION.jar"
 URI="CHK@/$FILENAME"
 
+echo "Installing pyFreenet3..."
+pip3 install -U --user --egg pyFreenet3
+
+# FIXME: As of 2018-05-03 fcpupload's "--spawn" parameter doesn't work,
+# see https://bugs.freenetproject.org/view.php?id=7018 - so we start our
+# own node.
+# Once that is fixed don't start a node here - do so by reverting the
+# commit which added this FIXME.
+
+cd "$TRAVIS_BUILD_DIR"/../fred
+echo "Configuring node..."
+
+# FIXME: Use fred's official URL once there is one
+wget https://github.com/ArneBab/lib-pyFreenet-staging/releases/download/spawn-ext-data/seednodes.fref
+
+cat << EOF > freenet.ini
+node.updater.enabled=false
+node.clientCacheType=ram
+node.storeType=ram
+fproxy.hasCompletedWizard=true
+security-levels.physicalThreatLevel=LOW
+security-levels.networkThreatLevel=LOW
+node.opennet.enabled=true
+node.outputBandwidthLimit=1048576
+node.inputBandwidthLimit=-1
+node.opennet.maxOpennetPeers=65
+node.load.threadLimit=1000
+logger.priority=NONE
+End
+EOF
+
+echo "Starting node..."
+java -Xmx512M -classpath 'build/output/*' -Djna.nosys=true freenet.node.NodeStarter &
+cd "$TRAVIS_BUILD_DIR"
+
+echo "Giving node 60s to boot..."
+sleep 60s
+
 echo "Uploading WoT JAR to $URI..."
-# TODO: As of 2018-05-30 fcpuploads "--timeout" doesn't work, using coreutils' timeout, try again later
-if ! timeout 10m fcpupload --spawn --fcpPort 9486 "$URI" "dist/WebOfTrust.jar" ; then
-	echo "Uploading WebOfTrust.jar to Freenet failed! Dumping wrapper.log, wrapper.conf and listing node dir..." >&2
-	echo "wrapper.log:" >&2
-	cat "$HOME/.local/share/babcom-spawn-9486/wrapper.log" >&2
-	echo "wrapper.conf:" >&2
-	cat "$HOME/.local/share/babcom-spawn-9486/wrapper.conf" >&2
-	echo "Node dir:" >&2
-	ls -alR "$HOME/.local/share/babcom-spawn-9486/" >&2
+# TODO: As of 2018-05-30 fcpupload's "--timeout" doesn't work, using coreutils' timeout, try again later
+if ! time timeout 10m fcpupload --wait "$URI" "$TRAVIS_BUILD_DIR/dist/WebOfTrust.jar" ; then
+	echo "Uploading WebOfTrust.jar to Freenet failed!" >&2
+	
+	# The commented out lines are for debugging fcpupload's "--spawn".
+	#
+	# echo "Uploading WebOfTrust.jar to Freenet failed! Dumping wrapper.log, wrapper.conf and listing node dir..." >&2
+	# echo "wrapper.log:" >&2
+	# cat "$HOME/.local/share/babcom-spawn-9486/wrapper.log" >&2
+	# echo "wrapper.conf:" >&2
+	# cat "$HOME/.local/share/babcom-spawn-9486/wrapper.conf" >&2
+	# echo "Node dir:" >&2
+	# ls -alR "$HOME/.local/share/babcom-spawn-9486/" >&2
+	
+	exit 1
+fi
+
+kill "$(jobs -p)"
+if ! wait ; then
+	echo "Node exit code indicates error!" >&2
 	exit 1
 fi
 
