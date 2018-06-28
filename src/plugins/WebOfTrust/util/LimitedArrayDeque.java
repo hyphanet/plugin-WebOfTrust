459142856afe884c81a3d280ccf9e5de355ca76a
xor-freenet <xor@freenetproject.org>
2018-06-29 00:10:11 +0200

    Add class LimitedArrayDeque
    Will be used by IdentityFileQueue.IdentityFileQueueStatistics to store a
    limited amount of statistics.
    
    Not making it implement any of the collection interfaces yet because we
    will only need a very small subset of their functions.

---
+++ src/plugins/WebOfTrust/util/LimitedArrayDeque.java
@@ -0,0 +1,49 @@
+package plugins.WebOfTrust.util;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
+/***
+ * A wrapper around class {@link ArrayDeque} which automatically obeys a size limit.
+ * Once the limit is reached adding an element to the tail will remove the head element. */
+public final class LimitedArrayDeque<T> implements Cloneable, Iterable<T> {
+
+	private final ArrayDeque<T> mQueue;
+
+	private final int mSizeLimit;
+
+
+	public LimitedArrayDeque(int sizeLimit) {
+		if(sizeLimit < 1)
+			throw new IllegalArgumentException("sizeLimit is < 1: " + sizeLimit);
+		
+		mQueue = new ArrayDeque<T>();
+		mSizeLimit = sizeLimit;
+	}
+
+	/** Copy-constructor for {@link #clone()}. */
+	private LimitedArrayDeque(LimitedArrayDeque<T> original) {
+		mQueue = original.mQueue.clone();
+		mSizeLimit = original.mSizeLimit;
+	}
+
+	public T addLast(T element) {
+		T result = null;
+		
+		if(mQueue.size() >= mSizeLimit)
+			result = mQueue.removeFirst();
+		
+		mQueue.addLast(element);
+		
+		return result;
+	}
+
+	@Override public LimitedArrayDeque<T> clone() {
+		return new LimitedArrayDeque<>(this);
+	}
+
+	@Override public Iterator<T> iterator() {
+		return mQueue.iterator();
+	}
+
+}
