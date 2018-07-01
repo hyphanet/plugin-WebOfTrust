3ebb42c7cba609c7a31e4882da321f0fca906df6
xor-freenet <xor@freenetproject.org>
2018-07-01 02:11:38 +0200

    Add class Pair
    Apparently Java still doesn't provide this as of 2018.
    
    Will be used by IdentityFileQueue.IdentityFileQueueStatistics to store
    input for an X/Y plot.
    
    Not obeying the WoT convention of prefixing the member variables with
    "m" because this is subject to be replaced with an external library
    class of e.g. Apache Java Commons once we bundle that.

---
+++ src/plugins/WebOfTrust/util/Pair.java
@@ -0,0 +1,12 @@
+package plugins.WebOfTrust.util;
+
+/** General purpose immutable 2-tuple. */
+public final class Pair<X, Y> {
+	public final X x;
+	public final Y y;
+
+	public Pair(X x, Y y) {
+		this.x = x;
+		this.y = y;
+	}
+}
