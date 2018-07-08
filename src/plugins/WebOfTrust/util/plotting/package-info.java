61ee77249d7f25f11c8ecbfc786d49206f5cd66f
xor-freenet <xor@freenetproject.org>
2018-07-08 02:22:47 +0200

    Create package util.plotting to prepare for moving stuff out of class StatisticsPage

---
+++ src/plugins/WebOfTrust/util/plotting/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Utility classes to preprocess data to make it suitable for plotting, and to plot it using the
+ * XChart library, e.g. as {@link org.knowm.xchart.XYChart}. */
+package plugins.WebOfTrust.util.plotting;
