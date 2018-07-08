3d5802f11faafe3a7778cd8ac72dc7a55bb46ab5
xor-freenet <xor@freenetproject.org>
2018-07-08 02:27:32 +0200

    Add class XYChartUtils for moving stuff out of StatisticsPage

---
+++ src/plugins/WebOfTrust/util/plotting/XYChartUtils.java
@@ -0,0 +1,10 @@
+package plugins.WebOfTrust.util.plotting;
+
+import org.knowm.xchart.XYChart;
+
+/**
+ * Utility classes to preprocess data to make it suitable for plotting, and to plot it using the
+ * XChart library's {@link XYChart}. */
+public final class XYChartUtils {
+
+}
