662e3cdd1dcd27c874d24cb4a0ca9f7d0218d8ac
xor-freenet <xor@freenetproject.org>
2018-10-09 09:26:45 +0200

    StatisticsPage: Remove already resolved FIXME
    Was fixed by:
    8213917f7913c1bf2aa7045403793dbaab1ea15a

--- src/plugins/WebOfTrust/ui/web/StatisticsPage.java
+++ src/plugins/WebOfTrust/ui/web/StatisticsPage.java
@@ -188,9 +188,6 @@ public class StatisticsPage extends WebPageImpl {
 				String l10n = "StatisticsPage.PlotBox.DownloadsPerHourPlot.";
 				
 				// FIXME: Add first/last value to input data which produces desirable results.
-				// Change getTimeBasedPlotPNG() to not add the trailing element which it added
-				// for purposes of TotalDownloadCount, that one should do it itself as it doesn't
-				// necessarily make sense for our plot here.
 				
 				// - Build the average before differentiating to prevent a jumpy graph due to
 				//   fred delivering batches of many files at once for internal reasons.
