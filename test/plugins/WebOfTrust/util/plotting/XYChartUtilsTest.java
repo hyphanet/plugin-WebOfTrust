d1a3a005dba9bfd260c0e33e2153191dae381475
xor-freenet <xor@freenetproject.org>
2018-10-18 17:05:54 +0200

    Add skeleton class XYChartUtilsTest

---
+++ test/plugins/WebOfTrust/util/plotting/XYChartUtilsTest.java
@@ -0,0 +1,28 @@
+package plugins.WebOfTrust.util.plotting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import plugins.WebOfTrust.AbstractJUnit4BaseTest;
+import plugins.WebOfTrust.WebOfTrust;
+
+public final class XYChartUtilsTest extends AbstractJUnit4BaseTest {
+
+	@Test public void testMovingAverage() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testDifferentiate() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testMultiplyY() {
+		fail("Not yet implemented");
+	}
+
+	@Override protected WebOfTrust getWebOfTrust() {
+		return null;
+	}
+
+}
