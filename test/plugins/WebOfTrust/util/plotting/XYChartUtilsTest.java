ffcd6ce2d7652a02ae6ddc7a47dbfcc6a32fdf77
xor-freenet <xor@freenetproject.org>
2018-10-18 17:42:18 +0200

    XYChartUtilsTest: Implement testDifferentiate()

--- test/plugins/WebOfTrust/util/plotting/XYChartUtilsTest.java
+++ test/plugins/WebOfTrust/util/plotting/XYChartUtilsTest.java
@@ -1,11 +1,22 @@
 package plugins.WebOfTrust.util.plotting;
 
-import static org.junit.Assert.*;
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static plugins.WebOfTrust.util.MathUtil.equalsApprox;
+import static plugins.WebOfTrust.util.plotting.XYChartUtils.differentiate;
+
+import java.util.Iterator;
 
 import org.junit.Test;
 
 import plugins.WebOfTrust.AbstractJUnit4BaseTest;
 import plugins.WebOfTrust.WebOfTrust;
+import plugins.WebOfTrust.util.Pair;
+import plugins.WebOfTrust.util.plotting.XYChartUtils.TimeChart;
 
 public final class XYChartUtilsTest extends AbstractJUnit4BaseTest {
 
@@ -14,7 +25,29 @@ public final class XYChartUtilsTest extends AbstractJUnit4BaseTest {
 	}
 
 	@Test public void testDifferentiate() {
-		fail("Not yet implemented");
+		TimeChart<Double> sinus = new TimeChart<>(1024);
+		
+		for(int i = 0; i < 1024; ++i) {
+			double x = (2*PI / 1024) * i;
+			double y = sin(x);
+			sinus.addLast(new Pair<>(x, y));
+			assertEquals(i+1, sinus.size());
+		}
+		
+		TimeChart<Double> differentials = differentiate(sinus);
+		assertEquals(sinus.size() - 1, differentials.size());
+		
+		Iterator<Pair<Double, Double>> iter = differentials.iterator();
+		for(int i = 0; i < 1024-1; ++i) {
+			double x1 = (2*PI / 1024) * i;
+			double x2 = (2*PI / 1024) * (i+1);
+			double x = (x1+x2) / 2;
+			double y = cos(x); // d/dx sin(x) = cos(x)
+			
+			Pair<Double, Double> p = iter.next();
+			assertTrue("expected ~ " + x + "; was: " + p.x, equalsApprox(x, p.x, 99.999d));
+			assertTrue("expected ~ " + y + "; was: " + p.y, equalsApprox(y, p.y, 99.999d));
+		}
 	}
 
 	@Test public void testMultiplyY() {
