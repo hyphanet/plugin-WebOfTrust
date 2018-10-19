3571ef0e48b9085a37e7feec62d6f23623075a40
xor-freenet <xor@freenetproject.org>
2018-10-19 10:51:56 +0200

    XYChartUtilsTest: Begin implementing testMovingAverage()
    While it is a bit ugly to list all the elements manually instead of
    creating them in a loop I've  nevertheless done so to have this serve as
    a proof of concept for the newly added convenience functions which aid
    manual creation - this style will certainly be indicated in other tests
    where manual data creation is necessary, so trying it out is OK.

--- test/plugins/WebOfTrust/util/plotting/XYChartUtilsTest.java
+++ test/plugins/WebOfTrust/util/plotting/XYChartUtilsTest.java
@@ -3,11 +3,15 @@ package plugins.WebOfTrust.util.plotting;
 import static java.lang.Math.PI;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static plugins.WebOfTrust.util.CollectionUtil.array;
 import static plugins.WebOfTrust.util.MathUtil.equalsApprox;
+import static plugins.WebOfTrust.util.Pair.pair;
 import static plugins.WebOfTrust.util.plotting.XYChartUtils.differentiate;
+import static plugins.WebOfTrust.util.plotting.XYChartUtils.movingAverage;
 
 import java.util.Iterator;
 
@@ -21,7 +25,35 @@ import plugins.WebOfTrust.util.plotting.XYChartUtils.TimeChart;
 public final class XYChartUtilsTest extends AbstractJUnit4BaseTest {
 
 	@Test public void testMovingAverage() {
-		fail("Not yet implemented");
+		TimeChart<Double> data = new TimeChart<>(128);
+		data.addAll(asList(array(
+			pair( 1d, 11d),
+			pair( 2d, 12d),
+			pair( 3d, 13d),
+			pair( 4d, 14d),
+			pair( 5d, 15d),
+			pair( 6d, 16d),
+			pair( 7d, 17d),
+			pair( 8d, 18d),
+			pair( 9d, 19d),
+			pair(10d, 20d),
+			pair(11d, 21d),
+			pair(12d, 22d),
+			pair(13d, 23d),
+			pair(14d, 24d),
+			pair(15d, 25d),
+			pair(16d, 26d)
+		)));
+		
+		TimeChart<Double> average = movingAverage(data, 1);
+		// For the first 16 elements only one average will be yielded as the minimum window size
+		// is not only the given amount of seconds but also 16 elements.
+		assertEquals(1, average.size());
+		Pair<Double, Double> a1 = average.peekFirst();
+		assertTrue(equalsApprox( 8.5d, a1.x, 99.999d));
+		assertTrue(equalsApprox(18.5d, a1.y, 99.999d));
+		
+		fail("Implement rest of this: Test what happens with more than 16 elements.");
 	}
 
 	@Test public void testDifferentiate() {
