2bbc5a6223416d6c6a046ec9bcbc3b15c752f291
xor-freenet <xor@freenetproject.org>
2018-10-20 12:34:35 +0200

    XYChartUtilsTest: Implement testMultiplyY()

--- test/plugins/WebOfTrust/util/plotting/XYChartUtilsTest.java
+++ test/plugins/WebOfTrust/util/plotting/XYChartUtilsTest.java
@@ -3,13 +3,17 @@ package plugins.WebOfTrust.util.plotting;
 import static java.lang.Math.PI;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static plugins.WebOfTrust.util.CollectionUtil.array;
 import static plugins.WebOfTrust.util.Pair.pair;
 import static plugins.WebOfTrust.util.plotting.XYChartUtils.differentiate;
 import static plugins.WebOfTrust.util.plotting.XYChartUtils.movingAverage;
+import static plugins.WebOfTrust.util.plotting.XYChartUtils.multiplyY;
 
 import java.util.Iterator;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -74,7 +78,39 @@ public final class XYChartUtilsTest extends AbstractJUnit4BaseTest {
 	}
 
 	@Test public void testMultiplyY() {
-		fail("Not yet implemented");
+		// TODO: Code quality: Java 8: Use "var" to shorten this and the other generics here
+		// - hence the bloatedness of this function for such a simple test, to remind me of Java 8
+		// patterns to learn.
+		TimeChart<Double> input = new TimeChart<>(5);
+		input.addAll(asList(array(
+			pair(1d, -2d),
+			pair(2d, -1d),
+			pair(3d,  0d),
+			pair(4d,  1d),
+			pair(5d,  2d)
+		)));
+		TimeChart<Double> actual = multiplyY(input, -10);
+		List<Pair<Double, Double>> expected = asList(array(
+			pair(1d,  20d),
+			pair(2d,  10d),
+			pair(3d,   0d),
+			pair(4d, -10d),
+			pair(5d, -20d)
+		));
+		
+		assertEquals(5, input.size());
+		assertEquals(5, actual.size());
+		// TODO: Code quality: Java 8: Lambda expressions would shorten this a lot:
+		// https://stackoverflow.com/a/37612232
+		// A pattern which will be worth learning for any occasion of iterating over two lists.
+		Iterator<Pair<Double, Double>> iExp = expected.iterator();
+		Iterator<Pair<Double, Double>> iAct = actual.iterator();
+		for(int i=0; i < 5; ++i) {
+			Pair<Double, Double> exp = iExp.next();
+			Pair<Double, Double> act = iAct.next();
+			assertEqualsApprox(exp.x, act.x, 99.999d);
+			assertEqualsApprox(exp.y, act.y, 99.999d);
+		}
 	}
 
 	@Override protected WebOfTrust getWebOfTrust() {
