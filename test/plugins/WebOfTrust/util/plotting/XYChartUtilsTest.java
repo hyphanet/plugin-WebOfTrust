c0b97d94db7c6b87d7acbe123530e95bb46d61dd
xor-freenet <xor@freenetproject.org>
2018-10-20 11:47:44 +0200

    XYChartUtilsTest.testMovingAverage(): Simplify
    - Use the inverse of the X-values as Y-values so they can also be
      calculated by the sum equation n(n+1)/2.
    - Don't calculate the X-/Y-values by hand.

--- test/plugins/WebOfTrust/util/plotting/XYChartUtilsTest.java
+++ test/plugins/WebOfTrust/util/plotting/XYChartUtilsTest.java
@@ -3,12 +3,8 @@ package plugins.WebOfTrust.util.plotting;
 import static java.lang.Math.PI;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static plugins.WebOfTrust.util.CollectionUtil.array;
-import static plugins.WebOfTrust.util.MathUtil.equalsApprox;
 import static plugins.WebOfTrust.util.Pair.pair;
 import static plugins.WebOfTrust.util.plotting.XYChartUtils.differentiate;
 import static plugins.WebOfTrust.util.plotting.XYChartUtils.movingAverage;
@@ -26,34 +22,29 @@ public final class XYChartUtilsTest extends AbstractJUnit4BaseTest {
 
 	@Test public void testMovingAverage() {
 		TimeChart<Double> data = new TimeChart<>(128);
-		data.addAll(asList(array(
-			pair( 1d, 11d),
-			pair( 2d, 12d),
-			pair( 3d, 13d),
-			pair( 4d, 14d),
-			pair( 5d, 15d),
-			pair( 6d, 16d),
-			pair( 7d, 17d),
-			pair( 8d, 18d),
-			pair( 9d, 19d),
-			pair(10d, 20d),
-			pair(11d, 21d),
-			pair(12d, 22d),
-			pair(13d, 23d),
-			pair(14d, 24d),
-			pair(15d, 25d),
-			pair(16d, 26d)
-		)));
+		// Use the first N natural numbers as input for the average as their sum can easily be
+		// calculated as:
+		//     sum(1, 2, 3, ..., N) = N*(N+1) / 2
+		// This equation is unnamed in English, it can be found by its German name
+		// "Gaußsche Summenformel".
+		for(int n = 1; n <= 16; ++n)
+			data.addLast(pair((double) n, (double) -n));
 		
 		TimeChart<Double> average = movingAverage(data, 1);
 		// For the first 16 elements only one average will be yielded as the minimum window size
 		// is not only the given amount of seconds but also 16 elements.
 		assertEquals(1, average.size());
 		Pair<Double, Double> a1 = average.peekFirst();
-		assertEqualsApprox( 8.5d, a1.x, 99.999d);
-		assertEqualsApprox(18.5d, a1.y, 99.999d);
-		
+		assertEqualsApprox( sumOfNumbers(16) / 16, a1.x, 99.999d);
+		assertEqualsApprox(-sumOfNumbers(16) / 16, a1.y, 99.999d);
 		fail("Implement rest of this: Test what happens with more than 16 elements.");
+	}
+
+	private static double sumOfNumbers(int n) {
+		assert(n >= 0);
+		int enumerator = n*(n+1);
+		assert(enumerator % 2 == 0) : "Integer division before casting is OK";
+		return enumerator / 2;
 	}
 
 	@Test public void testDifferentiate() {
