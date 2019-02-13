b0a7091633e95c5acb9548bbd67143d6196b72d8
xor-freenet <xor@freenetproject.org>
2019-02-13 12:55:26 +0100

    .travis.upload-jar-to-freenet.sh: Remove code which belongs into .travis.start-freenet.sh
    The scripts may work now.

--- .travis.upload-jar-to-freenet.sh
+++ .travis.upload-jar-to-freenet.sh
@@ -4,51 +4,12 @@ set -o errexit
 set -o errtrace
 trap 'echo "Error at line $LINENO, exit code $?" >&2' ERR
 
+"$TRAVIS_BUILD_DIR"/.travis.start-freenet.sh
+sleep 60s
+
 FILENAME="WebOfTrust-$(git describe --always)-built-on-$TRAVIS_JDK_VERSION.jar"
 URI="CHK@/$FILENAME"
-
-echo "Installing pyFreenet3..."
-pip3 install -U --user --egg pyFreenet3
-
-# FIXME: As of 2018-05-03 fcpupload's "--spawn" parameter doesn't work,
-# see https://bugs.freenetproject.org/view.php?id=7018 - so we start our
-# own node.
-# Once that is fixed don't start a node here - do so by reverting the
-# commit which added this FIXME.
-
-cd "$TRAVIS_BUILD_DIR"/../fred
-echo "Configuring node..."
-
-# FIXME: Use fred's official URL once there is one
-wget https://github.com/ArneBab/lib-pyFreenet-staging/releases/download/spawn-ext-data/seednodes.fref
-
-# Use a non-standard port to not interfere with WoT unit tests.
-# (They should not be connecting to FCP by network, but let's be paranoid anyway.)
 FCP_PORT=23874
-
-cat << EOF > freenet.ini
-fcp.port=$FCP_PORT
-node.updater.enabled=false
-node.clientCacheType=ram
-node.storeType=ram
-fproxy.hasCompletedWizard=true
-security-levels.physicalThreatLevel=LOW
-security-levels.networkThreatLevel=LOW
-node.opennet.enabled=true
-node.outputBandwidthLimit=1048576
-node.inputBandwidthLimit=-1
-node.opennet.maxOpennetPeers=65
-node.load.threadLimit=1000
-logger.priority=NONE
-End
-EOF
-
-echo "Starting node..."
-java -Xmx512M -classpath 'build/output/*' -Djna.nosys=true freenet.node.NodeStarter &
-cd "$TRAVIS_BUILD_DIR"
-
-echo "Giving node 60s to boot..."
-sleep 60s
 
 echo "Uploading WoT JAR to $URI..."
 
@@ -82,12 +43,8 @@ if ! time fcpupload --fcpPort=$FCP_PORT --wait --realtime \
 	exit 1
 fi
 
-# Can't use "kill $(jobs -p)" as that fails if a job exits in between
-jobs -p | xargs --no-run-if-empty kill || true
-
-if ! wait ; then
-	echo "Node exit code indicates error!" >&2
-	exit 1
-fi
+# TODO: Use pyFreenet for this
+echo "Removing Freenet's anchor file to tell it to shutdown..."
+rm "$TRAVIS_BUILD_DIR"/../fred/Freenet.anchor
 
 exit 0
