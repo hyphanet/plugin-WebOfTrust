921b00f678a11f64ea3de639ed43f3d93560bf05
xor-freenet <xor@freenetproject.org>
2019-09-15 04:30:35 +0200

    Add skeleton test class CollectionUtilTest

---
+++ test/plugins/WebOfTrust/util/CollectionUtilTest.java
@@ -0,0 +1,21 @@
+package plugins.WebOfTrust.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CollectionUtilTest {
+
+	@Test public void testArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testArrayList() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testIgnoreNulls() {
+		fail("Not yet implemented");
+	}
+
+}
