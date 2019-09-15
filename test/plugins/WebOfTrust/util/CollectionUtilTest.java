689654af52ecfa423cd2b9ce27187fb9f1d07fc1
xor-freenet <xor@freenetproject.org>
2019-09-15 05:25:51 +0200

    CollectionUtilTest: Implement testArray()

--- test/plugins/WebOfTrust/util/CollectionUtilTest.java
+++ test/plugins/WebOfTrust/util/CollectionUtilTest.java
@@ -1,13 +1,24 @@
 package plugins.WebOfTrust.util;
 
 import static org.junit.Assert.*;
+import static plugins.WebOfTrust.util.CollectionUtil.array;
 
 import org.junit.Test;
 
 public class CollectionUtilTest {
 
 	@Test public void testArray() {
-		fail("Not yet implemented");
+		Integer[] a = array(10, 20, 30);
+		assertEquals(3, a.length);
+		assertEquals(Integer.valueOf(10), a[0]);
+		assertEquals(Integer.valueOf(20), a[1]);
+		assertEquals(Integer.valueOf(30), a[2]);
+		
+		try {
+			array();
+			// See the JavaDoc of array() for why this is necessary.
+			fail("array() must throw IllegalArgumentException for empty varargs");
+		} catch(IllegalArgumentException e) {}
 	}
 
 	@Test public void testArrayList() {
