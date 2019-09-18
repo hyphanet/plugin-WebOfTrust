11eb0cedee11954d87c24cb33b3d38b6a943e034
xor-freenet <xor@freenetproject.org>
2019-09-18 21:34:59 +0200

    LimitedArrayDequeTest: Implement testConstructor()

--- test/plugins/WebOfTrust/util/LimitedArrayDequeTest.java
+++ test/plugins/WebOfTrust/util/LimitedArrayDequeTest.java
@@ -1,14 +1,36 @@
 package plugins.WebOfTrust.util;
 
 import static org.junit.Assert.*;
+import static plugins.WebOfTrust.util.AssertUtil.assertDidNotThrow;
+import static plugins.WebOfTrust.util.AssertUtil.assertDidThrow;
+
+import java.util.concurrent.Callable;
 
 import org.junit.Test;
 
 /** Tests {@link LimitedArrayDeque}. */
 public final class LimitedArrayDequeTest {
 
-	@Test public void testLimitedArrayDeque() {
-		fail("Not yet implemented");
+	@Test public void testConstructor() {
+		assertEquals(123, new LimitedArrayDeque<Integer>(123).sizeLimit());
+		
+		// Size limits less than 1 should not be accepted, test that...
+		// TODO: Java 8: Use lambda expressions instead of anonymous classes.
+		
+		assertDidThrow(new Callable<Object>() {
+		@Override public Object call() throws Exception {
+			return new LimitedArrayDeque<Integer>(-1);
+		}}, IllegalArgumentException.class);
+		
+		assertDidThrow(new Callable<Object>() {
+		@Override public Object call() throws Exception {
+			return new LimitedArrayDeque<Integer>(0);
+		}}, IllegalArgumentException.class);
+		
+		assertDidNotThrow(new Runnable() {
+		@Override public void run() {
+			 new LimitedArrayDeque<Integer>(1);
+		}});
 	}
 
 	@Test public void testAddFirst() {
