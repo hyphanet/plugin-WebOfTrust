d8f725bb0bb90da38c7cc89a5494aaceab3e581e
xor-freenet <xor@freenetproject.org>
2019-09-18 21:11:26 +0200

    Add skeleton test class LimitedArrayDequeTest
    Generated using Eclipse to make sure all methods are covered.

---
+++ test/plugins/WebOfTrust/util/LimitedArrayDequeTest.java
@@ -0,0 +1,66 @@
+package plugins.WebOfTrust.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** Tests {@link LimitedArrayDeque}. */
+public final class LimitedArrayDequeTest {
+
+	@Test public void testLimitedArrayDeque() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testAddFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testAddLast() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testAddAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testPeekFirst() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testPeekLast() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testSizeLimit() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testToArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+}
