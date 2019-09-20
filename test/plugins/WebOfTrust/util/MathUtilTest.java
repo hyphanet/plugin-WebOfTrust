1e811deaf8df0d7daf425cade18782ad32a3e3e9
xor-freenet <xor@freenetproject.org>
2019-09-21 01:12:53 +0200

    Add skeleton unit test class MathUtilTest

---
+++ test/plugins/WebOfTrust/util/MathUtilTest.java
@@ -0,0 +1,18 @@
+package plugins.WebOfTrust.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** Tests {@link MathUtil}. */
+public final class MathUtilTest {
+
+	@Test public void testEqualsApprox() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testInteger() {
+		fail("Not yet implemented");
+	}
+
+}
