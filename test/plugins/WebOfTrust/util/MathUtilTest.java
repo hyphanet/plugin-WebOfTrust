74db45de2fb89797c96b65dfb755c78e282b01cf
xor-freenet <xor@freenetproject.org>
2019-09-21 03:05:08 +0200

    MathUtilTest: Begin implementing testEqualsApprox()

--- test/plugins/WebOfTrust/util/MathUtilTest.java
+++ test/plugins/WebOfTrust/util/MathUtilTest.java
@@ -1,6 +1,7 @@
 package plugins.WebOfTrust.util;
 
 import static org.junit.Assert.*;
+import static plugins.WebOfTrust.util.MathUtil.equalsApprox;
 
 import org.junit.Test;
 
@@ -8,7 +9,20 @@ import org.junit.Test;
 public final class MathUtilTest {
 
 	@Test public void testEqualsApprox() {
-		fail("Not yet implemented");
+		final double a = 0.87654321d * Double.MAX_VALUE;
+		final double b = a*1.10d; // Ten percent deviation
+		
+		// Basic equality
+		
+		assertTrue( equalsApprox(a, b, 89d));
+		assertTrue( equalsApprox(b, a, 89d));
+		assertFalse(equalsApprox(a, b, 91d));
+		assertFalse(equalsApprox(b, a, 91d));
+		
+		// Special cases
+		
+		assertTrue(equalsApprox(a,   a, 99.999d));
+		assertTrue(equalsApprox(0d, 0d, 99.999d));
 	}
 
 	@Test public void testInteger() {
