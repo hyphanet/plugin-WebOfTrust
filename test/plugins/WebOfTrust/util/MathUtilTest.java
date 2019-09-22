41fec37ad5ec14e92d6963d61709bc34eb961947
xor-freenet <xor@freenetproject.org>
2019-09-22 02:00:33 +0200

    MathUtilTest: Continue implementing testEqualsApprox()

--- test/plugins/WebOfTrust/util/MathUtilTest.java
+++ test/plugins/WebOfTrust/util/MathUtilTest.java
@@ -1,7 +1,10 @@
 package plugins.WebOfTrust.util;
 
 import static org.junit.Assert.*;
+import static plugins.WebOfTrust.util.AssertUtil.assertDidThrow;
 import static plugins.WebOfTrust.util.MathUtil.equalsApprox;
+
+import java.util.concurrent.Callable;
 
 import org.junit.Test;
 
@@ -24,6 +27,22 @@ public final class MathUtilTest {
 		assertTrue( equalsApprox(a,   a, 99.999d));
 		assertFalse(equalsApprox(a,  -a, 99.999d));
 		assertTrue( equalsApprox(0d, 0d, 99.999d));
+		
+		// TODO: Java 8: Use lambda expressions instead of anonymous classes.
+		
+		assertDidThrow(new Callable<Boolean>() { @Override public Boolean call() throws Exception {
+			return equalsApprox(1d, Double.NaN, 90);
+		}}, ArithmeticException.class);
+		assertDidThrow(new Callable<Boolean>() { @Override public Boolean call() throws Exception {
+			return equalsApprox(Double.NaN, 1d, 90);
+		}}, ArithmeticException.class);
+		
+		assertDidThrow(new Callable<Boolean>() { @Override public Boolean call() throws Exception {
+			return equalsApprox(1d, Double.POSITIVE_INFINITY, 90);
+		}}, ArithmeticException.class);
+		assertDidThrow(new Callable<Boolean>() { @Override public Boolean call() throws Exception {
+			return equalsApprox(Double.POSITIVE_INFINITY, 1d, 90);
+		}}, ArithmeticException.class);
 	}
 
 	@Test public void testInteger() {
