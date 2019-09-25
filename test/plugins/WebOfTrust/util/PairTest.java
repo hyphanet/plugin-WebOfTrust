1398e41275f84e80c81890b83bfbef923ef51261
xor-freenet <xor@freenetproject.org>
2019-09-26 01:32:21 +0200

    Add skeleton unit test class PairTest
    Generated using Eclipse.

---
+++ test/plugins/WebOfTrust/util/PairTest.java
@@ -0,0 +1,26 @@
+package plugins.WebOfTrust.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** Tests {@link Pair}. */
+public final class PairTest {
+
+	@Test public void testConstructor1() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testConstructor2() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+}
