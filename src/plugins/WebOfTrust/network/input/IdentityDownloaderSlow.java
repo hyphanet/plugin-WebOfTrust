b8e0da3d7b238714129068b9cc9e0767db07c729
xor-freenet <xor@freenetproject.org>
2019-11-16 06:16:36 +0100

    IdentityDownloaderSlow: Add assert() to detect the semi-bug which the last commit was about

--- src/plugins/WebOfTrust/network/input/IdentityDownloaderSlow.java
+++ src/plugins/WebOfTrust/network/input/IdentityDownloaderSlow.java
@@ -590,6 +590,9 @@ public final class IdentityDownloaderSlow implements
 				Logger.error(this, "Error in run()! Retrying later...", e);
 				mDownloadSchedulerThread.triggerExecution(QUEUE_BATCHING_DELAY_MS);
 			} finally {
+				// TODO: Performance: Remove this after it has not failed for some time.
+				assert(getRunningDownloadCount() <= getQueue().size());
+				
 				if(logMINOR) Logger.minor(this, "run() finished.");
 			}
 		}
