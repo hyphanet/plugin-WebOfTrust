c8b3b7673d3ad07eb16d4fa78926ea2feca8995b
xor-freenet <xor@freenetproject.org>
2020-01-12 03:34:46 +0100

    Revert "WebOfTrust.deleteDuplicateObjects(): Add assert()s so it causes unit tests to fail upon duplicates"
    This reverts commit 8d914b39bb43b684aa57412f8000321294a4a148.
    
    Would cause WebOfTrustTest.testDeleteDuplicateObjects() to fail without
    a reason to do so.
    The proper fix for the intention of the reverted commit is to implement
    what is proposed here:
    https://bugs.freenetproject.org/view.php?id=7126

--- src/plugins/WebOfTrust/WebOfTrust.java
+++ src/plugins/WebOfTrust/WebOfTrust.java
@@ -1412,7 +1412,6 @@ public final class WebOfTrust extends WebOfTrustInterface
 			for(Identity i : getAllIdentities()) {
 				if(!identitySet.add(i)) {
 					Logger.error(this, "Deleting duplicate Identity: " + i);
-					assert(false) : i;
 					deleteWithoutCommit(i);
 					anythingChanged = true;
 				}
@@ -1427,7 +1426,6 @@ public final class WebOfTrust extends WebOfTrustInterface
 			for(Trust t : getAllTrusts()) {
 				if(!trustSet.add(t)) {
 					Logger.error(this, "Deleting duplicate trust: " + t);
-					assert(false) : t;
 					t.deleteWithoutCommit();
 					anythingChanged = true;
 				}
@@ -1437,7 +1435,6 @@ public final class WebOfTrust extends WebOfTrustInterface
 			for(Score s : getAllScores()) {
 				if(!scoreSet.add(s)) {
 					Logger.error(this, "Deleting duplicate Score: " + s);
-					assert(false) : s;
 					s.deleteWithoutCommit();
 					anythingChanged = true;
 				}
