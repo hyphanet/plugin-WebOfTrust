ebe8f2f18bb9d5dd614f3f253e81a0ff148c5528
xor-freenet <xor@freenetproject.org>
2020-02-29 05:51:41 +0100

    XMLTransformer: Replace usage of deprecated function
    Improves readability of the
        mWoT.getIdentityDownloaderController().onNewEditionImported(identity);
    inside of the synchronization block.

--- src/plugins/WebOfTrust/XMLTransformer.java
+++ src/plugins/WebOfTrust/XMLTransformer.java
@@ -747,7 +747,7 @@ public final class XMLTransformer {
 		catch(Exception e) {
 			synchronized(mWoT) {
 			// synchronized(mSubscriptionManager) { // We don't use the SubscriptionManager, see below
-			synchronized(mWoT.getIdentityFetcher()) {
+			synchronized(mWoT.getIdentityDownloaderController()) {
 			synchronized(Persistent.transactionLock(mDB) ) {
 				// FIXME: build0020 lacked a try/catch block to rollback the transaction upon error
 				// even though the old version of this code block also did multiple modifications to
