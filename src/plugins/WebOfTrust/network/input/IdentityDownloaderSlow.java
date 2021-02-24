7c47c44c36a1af5626bd6a0fe070225f64c25b3c
xor-freenet <xor@freenetproject.org>
2021-02-24 23:23:57 +0100

    IdentityDownloaderSlow: Documentation

--- src/plugins/WebOfTrust/network/input/IdentityDownloaderSlow.java
+++ src/plugins/WebOfTrust/network/input/IdentityDownloaderSlow.java
@@ -638,6 +638,16 @@ public final class IdentityDownloaderSlow implements
 				//   a faster discovery of all remote identities because the more trust lists we
 				//   download from different identities the higher the chance of discovering new
 				//   ones in them.
+				// - Because we download more diverse trust lists we also might get more fresh hints
+				//   from the more various Identitys we download and thus a higher chance of
+				//   avoiding usage of obsolete edition hints, i.e. hints for editions which
+				//   are lower than the actual most recent edition of an Identity.
+				//   (This is a weak conclusion though, EditionHint.getPriority(), which sorts our
+				//   download queue, uses their date as the first sorting key. And while this
+				//   currently is the date when they were gathered from trust lists and thus is
+				//   mostly the same for new databases, in a near update remote Identitys will be
+				//   allowed to propagate the date on which they observed a hint to defer usage of
+				//   old hints. Nevertheless lets document this here for future consideration.)
 				// (We could avoid having to ignore the Identitys here by instead ensuring that
 				// no EditionHints are stored which have almost the same priority as others.
 				// However ignoring them here is a lot easier than changing the storage
