ca97adfda59b9f46258337097f5f4d11bfc62a72
xor-freenet <xor@freenetproject.org>
2021-10-15 12:18:41 +0200

    XMLTransformer.importIdentity(): Fix IllegalStateException
    Trying to import an edition which was previously imported and
    marked with a FetchState of ParsingFailed but for which parsing did
    now succeed would result in:
    > java.lang.IllegalStateException: onFetched() called for old edition!
    >   Passed edition: 217; Last fetched edition: 217
    > at plugins.WebOfTrust.Identity.onFetched(Identity.java:710)
    > at plugins.WebOfTrust.Identity.onFetchedAndParsedSuccessfully(Identity.java:676)
    > at plugins.WebOfTrust.XMLTransformer.importIdentity(XMLTransformer.java:518)
    
    And the throwing is a correct implementation of Identity.onFetched()
    IMHO: It rightfully detects that it was already called for that edition.
    
    Losing the hereby removed branch of code is not an issue:
    
    - The proper approach of implementing re-fetching of an edition for
      which parsing failed is setting the FetchState to "NotFetched"
      instead of keeping it at ParsingFailed so the database actually tells
      the IdentityDownloader to download it again.
      We can safely assume that this proper approach will be chosen in
      future code because it is likely to use Identity.markForRefetch(), and
      that function was changed by commit
      fb04bb371ad82ddfa786891936ee04b8922d7a48 to not keep a previous
      FetchState of ParsingFailed but change it to NotFetched instead.
    
    - With regards to existing user databases where the FetchState might
      still be ParsingFailed for some Identitys which ought to be refetched:
      Commit 58de49821b69e9bdc0e8f6ea48214feb5b601a21 shipped database
      upgrade code to set the FetchState to NotFetched for all Identitys
      where it was ParsingFailed.
    
    - The fact that the above stack trace happened for me likely was not
      because the code path is assumed to exist by some other code (I'm
      quite confident I never wrote any WoT code which does that).
      Instead, my test runs had been lacking the aforementioned fix for
      markForRefetch(). If Trust-changes - which can happen at any time and
      are unrelated to this code path - caused markForRefetch() to be called
      upon an Identity which was marked as ParsingFailed it would decrease
      the edition of the Identity but keep the FetchState at ParsingFailed.
      The previous edition might be parseable so if something downloaded it
      again we would run into this stack trace.
      And that could indeed happen due to:
      - the previous lack of code in IdentityDownloaderSlow to prevent it
        from downloading multiple editions of the same Identity in parallel.
        See: https://bugs.freenetproject.org/view.php?id=6940
      - the concurrent operation of IdentityDownloaderFast and *Slow
        meaning they could download multiple editions of the same Identity
        in parallel.

--- src/plugins/WebOfTrust/XMLTransformer.java
+++ src/plugins/WebOfTrust/XMLTransformer.java
@@ -500,27 +500,18 @@ public final class XMLTransformer {
 				// Different implementations of IdentityDownloader may concurrently start a download
 				// for the same edition which can cause it to be downloaded twice.
 				// See the comments in IdentityDownloaderSlow.onNewEditionImported().
-				// TODO: Decide what to do about this logging, we shouldn't completely ignore
+				// FIXME: Decide what to do about this logging, we shouldn't completely ignore
 				// the issue if it happens too frequently so just removing the logging may not be
 				// a good idea. On the other hand keeping it also isn't good because the WARNING
 				// loglevel does sound too bad.
 				// Perhaps replace the logging with a counter of how often this happens, visible
 				// on the StatisticsPage?
 				
-				if(identity.getCurrentEditionFetchState() == FetchState.ParsingFailed) {
-					Logger.warning(this,
-						  "Fetched obsolete edition! Allowing import nevertheless because "
-						+ "current one is marked as ParsingFailed: This may not be a bug if you "
-						+ "intentionally cause re-fetching for cleanup after parser bugfixes! "
-						+ "edition: " + newEdition + "; " + identity);
-					
-					// Don't return, proceed to import it, maybe parsing works now.
-				} else {
-					Logger.warning(this,
-						"Fetched obsolete edition! Edition: " + newEdition + "; " + identity);
-					stats.mImportTime = null;
-					return stats;
-				}
+				
+				Logger.warning(this,
+					"Fetched obsolete edition! Edition: " + newEdition + "; " + identity);
+				stats.mImportTime = null;
+				return stats;
 			}
 			
 			// We throw parse errors AFTER checking the edition number: If this XML was outdated anyway, we don't have to throw.
